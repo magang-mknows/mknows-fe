e059f65773079e3deefd25b6b08b51fc809c6721
Fenny Oktaviani <fnny04@gmail.com>
2023-04-29 16:39:11 +0700

    create login pages

--- apps/cs/modules/auth/login/api.ts
+++ apps/cs/modules/auth/login/api.ts
@@ -1,1 +1,9 @@
-export {};
+import api from '../../../service/api';
+import { TLoginData, TLoginPayload } from './types';
+
+export const loginRequest = async (
+  payload: TLoginPayload
+): Promise<TLoginData> => {
+  const { data } = await api.post('/auth/login', payload);
+  return data;
+};
---
+++ apps/cs/service/api.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import type { AxiosRequestConfig } from 'axios';
+// import { getSession } from 'next-auth/react';
+
+const config: AxiosRequestConfig = {
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+};
+
+const api = axios.create(config);
+
+// api.interceptors.request.use(
+//   async (config) => {
+//     const session = await getSession();
+
+//     const token = session?.user?.token?.access_token;
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//       config.headers['Content-Type'] = 'application/json';
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+export default api;
