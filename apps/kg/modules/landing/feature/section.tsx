ea3e85633185fe7b4afdcc1a03928a9fb8d5f606
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-04-29 17:46:40 +0700

    improve: initial landing

--- apps/kg/modules/landing/feature/section.tsx
+++ apps/kg/modules/landing/feature/section.tsx
@@ -2,6 +2,6 @@ import { FC, ReactElement } from 'react';
 
 export const FeatureSection: FC = (): ReactElement => {
   return (
-    <section className="flex w-full h-[200px] justify-between bg-primary-500"></section>
+    <section className="flex w-full h-[140px] justify-between bg-primary-500"></section>
   );
 };
