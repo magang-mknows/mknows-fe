ebd20728d16d1f1965e34a27ecad0c6761589adb
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-04-29 18:01:08 +0700

    improve: test

--- apps/kg/modules/landing/feature/section.tsx
+++ apps/kg/modules/landing/feature/section.tsx
@@ -2,6 +2,8 @@ import { FC, ReactElement } from 'react';
 
 export const FeatureSection: FC = (): ReactElement => {
   return (
-    <section className="flex w-full h-[140px] justify-between bg-primary-500"></section>
+    <section className="flex w-full h-[140px] justify-between bg-primary-500">
+      Test
+    </section>
   );
 };
