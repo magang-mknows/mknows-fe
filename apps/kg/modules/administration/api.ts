68407548bfce14428121c1f684826d131e95c49a
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-05-04 13:56:06 +0700

    improvement: integration post biodata administration

--- apps/kg/modules/administration/api.ts
+++ apps/kg/modules/administration/api.ts
@@ -1,1 +1,15 @@
-export {};
+import api from '../../services/api';
+import { TBiodataAdm, TBiodataResponse } from './types';
+
+export const privateInformationRequest = async (
+  payload: TBiodataAdm
+): Promise<TBiodataResponse> => {
+  const { data } = await api.post('/administration/biodata', payload);
+  return data;
+};
+
+export const privateInformationGetRequest =
+  async (): Promise<TBiodataResponse> => {
+    const { data } = await api.get('/administration');
+    return data;
+  };
