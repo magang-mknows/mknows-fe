52a06c4c2782c1c49050ad835e786d20de75cea2
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-07 19:01:54 +0700

    improvement : add sidebar in index

--- apps/lxp/modules/my-training-discussion/index.tsx
+++ apps/lxp/modules/my-training-discussion/index.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { FC, ReactElement } from 'react';
+import { ClientProvider } from '../common/provider';
+import Sidebar from './sidebar';
 
-const index = () => {
-  return <div>index</div>;
+const MyTrainingDiscussionModule: FC = (): ReactElement => {
+  return (
+    <ClientProvider className="!bg-[#FAFAFA]">
+      <div className="w-full flex md:flex-row flex-col gap-10 px-6 md:px-8 lg:px-10 py-5   dark:bg-[#222529]">
+        <div className="md:basis-3/12 ">
+          <Sidebar />
+        </div>
+      </div>
+    </ClientProvider>
+  );
 };
 
-export default index;
+export default MyTrainingDiscussionModule;
