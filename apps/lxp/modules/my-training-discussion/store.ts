b728bc9ffeb7111d21b9f5a47769c261e652a038
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-07 19:10:53 +0700

    create atom popup modal schedule

--- apps/lxp/modules/my-training-discussion/store.ts
+++ apps/lxp/modules/my-training-discussion/store.ts
@@ -44,3 +44,8 @@ export const PopupModalSuccessOpen = atom({
   key: 'modal-popup-success',
   default: false,
 });
+
+export const PopupModalScheduleOpen = atom({
+  key: 'modal-popup-schedule',
+  default: false,
+});
