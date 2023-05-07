bd05dbb88d2be75411c33bb45f6f0bb3478a0823
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-05-07 22:53:12 +0700

    improvment:global input default border

--- apps/lxp/pages/_app.tsx
+++ apps/lxp/pages/_app.tsx
@@ -1,10 +1,13 @@
-// import './styles.css';
+import './styles.css';
 import 'tailwindcss/tailwind.css';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
+import { RecoilEnv, RecoilRoot } from 'recoil';
 import { SessionProvider } from 'next-auth/react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { Montserrat } from 'next/font/google';
+import { Suspense } from 'react';
+import { LoadingSpinner } from '@mknows-frontend-services/components/atoms';
 
 const queryClient = new QueryClient();
 
@@ -12,16 +15,21 @@ const montserrat = Montserrat({
   weight: '400',
   subsets: ['latin'],
 });
+
 function CustomApp({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
+
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
         <RecoilRoot>
           <main className={montserrat.className}>
-            <Component {...pageProps} />
+            <Suspense fallback={<LoadingSpinner />}>
+              <Component {...pageProps} />
+            </Suspense>
           </main>
         </RecoilRoot>
       </SessionProvider>
