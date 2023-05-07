2c5f526ff9cbb6f899907efb5f99ee526c19759f
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-07 19:26:29 +0700

    improvement : create module my-training component done

--- apps/lxp/modules/my-training-discussion/common/likeComment.tsx
+++ apps/lxp/modules/my-training-discussion/common/likeComment.tsx
@@ -2,24 +2,28 @@ import { FC, ReactElement, useState } from "react";
 import { AiFillLike } from "react-icons/ai";
 import Image from "next/image";
 
-const LikeComment: FC = (): ReactElement => {
+export const LikeComment: FC = (): ReactElement => {
   const [liked, setLiked] = useState(false);
   return (
     <>
       <div className="like-comment flex gap-5 text-[#737373] font-[500] text-[14px]">
         <div className="flex flex-row mt-8 gap-1 items-center">
           <button onClick={() => setLiked(liked === true ? false : true)}>
-            <AiFillLike className={`${liked === true ? "text-[#106FA4]" : ""} text-[18px]`} />
+            <AiFillLike
+              className={`${
+                liked === true ? 'text-[#106FA4]' : ''
+              } text-[18px]`}
+            />
           </button>
-          {liked === true ? "1 Suka" : "Suka"}
+          {liked === true ? '1 Suka' : 'Suka'}
         </div>
         <p className="flex flex-row mt-8 gap-1 items-center">
           <Image
-            src={"/assets/pelatihanku/comment.svg"}
+            src={'/assets/pelatihanku/comment.svg'}
             width={100}
             height={100}
             className="w-auto"
-            alt={"comment"}
+            alt={'comment'}
           />
           Balas
         </p>
@@ -28,4 +32,4 @@ const LikeComment: FC = (): ReactElement => {
   );
 };
 
-export default LikeComment;
+
--- apps/lxp/modules/my-training-discussion/index.tsx
+++ apps/lxp/modules/my-training-discussion/index.tsx
@@ -1,13 +1,17 @@
 import { FC, ReactElement } from 'react';
 import { ClientProvider } from '../common/provider';
 import Sidebar from './sidebar';
+import Content from './content';
 
 const MyTrainingDiscussionModule: FC = (): ReactElement => {
   return (
     <ClientProvider className="!bg-[#FAFAFA]">
-      <div className="w-full flex md:flex-row flex-col gap-10 px-6 md:px-8 lg:px-10 py-5   dark:bg-[#222529]">
+      <div className="w-full flex md:flex-row flex-col gap-10 px-6 md:px-8 lg:px-10 py-5 ">
         <div className="md:basis-3/12 ">
           <Sidebar />
+        </div>
+        <div>
+          <Content />
         </div>
       </div>
     </ClientProvider>
