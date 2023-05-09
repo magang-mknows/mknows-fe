0253e33db0cebce43f72e4c2e84d8d19485365b1
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-05-10 01:20:27 +0700

    feature:migrate discussion room lxp

---
+++ apps/lxp/modules/discussion-room/content/post/modal/index.ts
@@ -0,0 +1,4 @@
+export * from './delete-comfirm';
+export * from './post-report';
+export * from './post-report-success';
+export * from './post-spam';
---
+++ apps/lxp/modules/discussion-room/content/store.ts
@@ -0,0 +1,26 @@
+import { atom } from 'recoil';
+
+export const selectedOption = atom({
+  key: 'post-option',
+  default: '',
+});
+
+export const isModalOpen = atom({
+  key: 'modal-opened',
+  default: false,
+});
+
+export const reportDetailTitle = atom({
+  key: 'report-detail-title',
+  default: '',
+});
+
+export const reportSuccess = atom({
+  key: 'report-success',
+  default: false,
+});
+
+export const selectedPostId = atom({
+  key: 'post-id',
+  default: '',
+});
