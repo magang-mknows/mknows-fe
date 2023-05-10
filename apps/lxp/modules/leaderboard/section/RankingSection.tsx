f547edec29fd53ea5b0110a663442daf0ccff5ed
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-10 14:27:16 +0700

    fix darkmode in rangking section

--- apps/lxp/modules/leaderboard/section/RankingSection.tsx
+++ apps/lxp/modules/leaderboard/section/RankingSection.tsx
@@ -27,7 +27,7 @@ const RankingSection: FC = (): ReactElement => {
               <>
                 <div key={y}>
                   <div className="flex lg:px-[56px] px-3 mt-[15px] ">
-                    <div className="w-full dark:bg-[#222529] bg-white flex justify-between dark:border-b-[#222529] border-b-[#F5F5F5] border-b-2 px-[20px] rounded-[8px]">
+                    <div className="w-full bg-white flex justify-between  border-b-[#F5F5F5] border-b-2 px-[20px] rounded-[8px]">
                       <div className="flex items-center gap-[30px] p-2">
                         <h1 className="font-[400] lg:text-[24px] text-[20px]">
                           {x.index}
