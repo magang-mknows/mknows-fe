087bce2b2a55b773bfa1897c9abdc702310bd840
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-05-10 15:15:37 +0700

    improvement: migrate registration guide book

---
+++ apps/kg/pages/panduan/buku-panduan/panduan-pendaftaran/index.tsx
@@ -0,0 +1,8 @@
+import { RegistrationGuideBookModule } from 'apps/kg/modules/guide/registration-book';
+import { ReactElement } from 'react';
+
+const RegistrationBookPages = (): ReactElement => {
+  return <RegistrationGuideBookModule />;
+};
+
+export default RegistrationBookPages;
