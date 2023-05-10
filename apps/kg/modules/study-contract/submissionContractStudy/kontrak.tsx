a6360bbcb0da64ea9f4e17e59ab591a626835ab3
Salsabila Dita Prasetya <avocadobanana33@gmail.com>
2023-05-10 15:45:55 +0700

    improvement: fix button

--- apps/kg/modules/study-contract/submissionContractStudy/kontrak.tsx
+++ apps/kg/modules/study-contract/submissionContractStudy/kontrak.tsx
@@ -1,15 +1,15 @@
-import { FC, ReactElement } from "react";
-import Confirm from "./asset/Confirm.svg";
-import { Button } from "@mknows-frontend-services/components/atoms";
-import { PopupModal } from "@mknows-frontend-services/components/molecules";
-import Image from "next/image";
+import { FC, ReactElement } from 'react';
+import Confirm from './asset/Confirm.svg';
+import { Button } from '@mknows-frontend-services/components/atoms';
+import { PopupModal } from '@mknows-frontend-services/components/molecules';
+import Image, { StaticImageData } from 'next/image';
 // import GlobalButton from "../Common/GlobalButton";
-import {GlobalButton} from "@mknows-frontend-services/components/molecules";
-import { useDataTable } from "./hook";
-import { useDataCard } from "./hook";
-import Download from "./asset/download1.svg";
-import Warning from "./asset/warning.svg";
-import { usePopupConfirmCardStudy } from "./hook";
+import { GlobalButton } from '@mknows-frontend-services/components/molecules';
+import { useDataTable } from './hook';
+import { useDataCard } from './hook';
+import Download from './asset/download1.svg';
+import Warning from './asset/warning.svg';
+import { usePopupConfirmCardStudy } from './hook';
 // import SuspenseError from "@/modules/Common/SuspenseError";
 // import Loading from "../Loading";
 
@@ -31,8 +31,10 @@ const SubmissionContractStudy: FC = (): ReactElement => {
                 className="text-center lg:!w-44 !w-36 lg:!h-14 !h-10 lg:text-lg text-md py-4 "
                 text="+ Mengajukan"
                 onClick={() => setPopupStatus(true)}
-                hasImg={false} type={"button"} 
-                color="green"             />
+                hasImg={false}
+                type={'button'}
+                color="green"
+              />
             </div>
           </div>
           {/*isi span dari API */}
@@ -50,12 +52,12 @@ const SubmissionContractStudy: FC = (): ReactElement => {
       </div>
       <div className="flex justify-end items-center ">
         <Button
-          className="lg:text-center text-start text-md mx-4 lg:ml-80 "
-          size="regular"
-          color="greenBorder"
-          text="Download Silabus"
-          hasImg={true}
-          icon={Download} type={"button"}        />
+          className="flex gap-x-2 rounded justify-center items-center hover:opacity-50 duration-1000 lg:text-center text-start text-md mx-4 lg:ml-80 w-full h-[42px] lg:w-[328px] lg:h-[56px] text-[16px] font-medium bg-transparent text-[#3EB449] border-[#3EB449] border-2 disabled:text-[#A3A3A3] disabled:border-[#A3A3A3]"
+          type={'button'}
+        >
+          <Image alt="download" src={Download  as StaticImageData} />
+          Download Silabus
+        </Button>
       </div>
       <div className="px-4 mx-auto md:w-full">
         <div className="my-6 flex mx-4">
@@ -95,7 +97,9 @@ const SubmissionContractStudy: FC = (): ReactElement => {
                     <Image src={x.img} alt="User" />
                     <div className="flex w-full flex-col w-auto">
                       <h1 className="pt-3 font-bold text-start ">{x.matkul}</h1>
-                      <p className="pt-3 text-gray-400 text-start">{x.jmlh_mahasiswa}</p>
+                      <p className="pt-3 text-gray-400 text-start">
+                        {x.jmlh_mahasiswa}
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -118,30 +122,30 @@ const SubmissionContractStudy: FC = (): ReactElement => {
       </div>
 
       {/* <SuspenseError loadingFallback={<Loading />}> */}
-        <PopupModal
-          onClose={() => setPopupStatus(false)}
-          icon={Confirm}
-          popupTitle="Konfirmasi"
-          lookup={getPopupStatus}
-          className="!h-80 lg:!w-[100%] text-md py-10"
+      <PopupModal
+        onClose={() => setPopupStatus(false)}
+        icon={Confirm}
+        popupTitle="Konfirmasi"
+        lookup={getPopupStatus}
+        className="!h-80 lg:!w-[100%] text-md py-10"
+      >
+        <h1 className="py-2">
+          Kamu akan mengajukan program study{' '}
+          <span className="font-bold">Software Engineering</span>
+        </h1>
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert"
         >
-          <h1 className="py-2">
-            Kamu akan mengajukan program study{" "}
-            <span className="font-bold">Software Engineering</span>
-          </h1>
-          <div
-            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-            role="alert"
-          >
-            <div className="flex gap-2">
-              <Image src={Warning} alt="warning" />
-              <span className="block sm:inline">
-                Kamu tidak dapat mengganti Program Studi yang telah dipilih
-              </span>
-            </div>
+          <div className="flex gap-2">
+            <Image src={Warning} alt="warning" />
+            <span className="block sm:inline">
+              Kamu tidak dapat mengganti Program Studi yang telah dipilih
+            </span>
           </div>
-          <div className="flex gap-3 my-2 py-4">
-            {/* <Button
+        </div>
+        <div className="flex gap-3 my-2 py-4">
+          {/* <Button
               text="Batal"
               className="lg:w-[230px] lg:h-[56px] w-[100px] h-[56px] h border-[#106FA4] border-2 rounded-[8px] text-[#106FA4] "
               onClick={() => setPopupStatus(false)}
@@ -153,12 +157,11 @@ const SubmissionContractStudy: FC = (): ReactElement => {
               text="Konfirmasi"
               page={"/kontrak-krs/detail"}
             /> */}
-          </div>
-        </PopupModal>
+        </div>
+      </PopupModal>
       {/* </SuspenseError> */}
     </div>
   );
 };
 
 export default SubmissionContractStudy;
-
