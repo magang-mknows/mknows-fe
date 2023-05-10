3fe8794c22c5334e97393c865506118acbc0e709
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-10 14:26:58 +0700

    fix dark mode text

--- apps/lxp/modules/leaderboard/section/ContentSection.tsx
+++ apps/lxp/modules/leaderboard/section/ContentSection.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement } from 'react';
 import Image from 'next/image';
 import Medal1 from '../assets/medal-1.svg';
 import Medal3 from '../assets/medal-3.svg';
+import Medal from '../assets/medal.svg';
+import Crown from '../assets/crown.svg';
 import dynamic from 'next/dynamic';
 import {
   usePopupGetUser,
@@ -18,8 +20,8 @@ export const ContentSection = (): ReactElement => {
   const { setPopupUser } = usePopupGetUser();
   const { getRank } = useRankLeaderboard();
   return (
-    <div className="mt-[71px] w-full lg:px-[130px] md:px-[100px] px-3  mb-10 dark:bg-[#222529]">
-      <h1 className="text-center dark:text-white text-black text-28px font-[700] rounded-[8px]">
+    <div className="mt-[71px] w-full lg:px-[130px] md:px-[100px] px-3  mb-10 ">
+      <h1 className="text-center  text-black text-28px font-[700] rounded-[8px]">
         Papan Skor
       </h1>
       <div className="card bg-[#FFFF] mt-[39px] w-full rounded-[8px] py-10 ">
@@ -35,7 +37,7 @@ export const ContentSection = (): ReactElement => {
             .map((item, index) => {
               return index == 1 ? (
                 <div className="relative lg:-top-72 md:-top-60 sm:-top-52 -top-44 order-1">
-                  <div className="bg-[#FEEAA2] dark:bg-[#17A2B8] lg:h-[209px] lg:w-[190px] md:h-[180px] md:w-[165px] sm:h-[145px] sm:w-[150px] h-[125px] w-[120px] text-center rounded-tr-[8px] rounded-tl-[8px]">
+                  <div className="bg-[#FEEAA2]  lg:h-[209px] lg:w-[190px] md:h-[180px] md:w-[165px] sm:h-[145px] sm:w-[150px] h-[125px] w-[120px] text-center rounded-tr-[8px] rounded-tl-[8px]">
                     <div className="absolute lg:top-[40px] lg:left-[40px] md:top-[50px] md:left-[32px] sm:top-[20px] sm:left-[34px] top-6 left-7">
                       <div className="rounded-full outline outline-4 outline-[#67A5C8]">
                         <Image
@@ -44,7 +46,7 @@ export const ContentSection = (): ReactElement => {
                           className="lg:w-28 lg:h-28 md:w-24 md:h-24 sm:w-20 sm:h-20 w-16 h-16"
                         />
                         <Image
-                          src={'/assets/leaderboard/medal.svg'}
+                          src={Medal}
                           width={50}
                           height={50}
                           alt={'medal'}
@@ -64,7 +66,7 @@ export const ContentSection = (): ReactElement => {
                 </div>
               ) : index == 0 ? (
                 <div className="relative lg:-top-72 md:-top-60 sm:-top-52 -top-44 order-2">
-                  <div className="bg-[#FEF6D0] dark:bg-[#61b3bf]   lg:h-[374px] lg:w-[204px] md:h-[280px] md:w-[180px] sm:h-[240px] sm:w-[150px] h-[220px] w-[120px] text-center rounded-tr-[8px] rounded-tl-[8px] ">
+                  <div className="bg-[#FEF6D0]  lg:h-[374px] lg:w-[204px] md:h-[280px] md:w-[180px] sm:h-[240px] sm:w-[150px] h-[220px] w-[120px] text-center rounded-tr-[8px] rounded-tl-[8px] ">
                     <div className="absolute lg:top-[280px] lg:left-[40px] md:top-[180px] md:left-[36px] sm:top-[140px] sm:left-[32px] top-36 left-7">
                       <div className="rounded-full lg:w-32 lg:h-32 md:w-[105px] md:h-[105px] outline outline-4 outline-[#FBCB50] ">
                         <Image
@@ -77,7 +79,7 @@ export const ContentSection = (): ReactElement => {
                           }}
                         />
                         <Image
-                          src={'/assets/leaderboard/crown.svg'}
+                          src={Crown}
                           width={50}
                           height={50}
                           alt="crown"
@@ -102,7 +104,7 @@ export const ContentSection = (): ReactElement => {
                 </div>
               ) : index == 2 ? (
                 <div className="relative lg:-top-72 md:-top-60 sm:-top-52 -top-44 order-3">
-                  <div className="bg-[#FEEAA2] dark:bg-[#17A2B8] lg:h-[209px] lg:w-[190px] md:h-[180px] md:w-[165px] sm:h-[145px] sm:w-[150px] h-[125px] w-[120px] text-center rounded-tr-[8px] rounded-tl-[8px]">
+                  <div className="bg-[#FEEAA2]  lg:h-[209px] lg:w-[190px] md:h-[180px] md:w-[165px] sm:h-[145px] sm:w-[150px] h-[125px] w-[120px] text-center rounded-tr-[8px] rounded-tl-[8px]">
                     <div className="absolute lg:top-[40px] lg:left-[40px] md:top-[50px] md:left-[32px] sm:top-[20px] sm:left-[34px] top-6 left-7">
                       <div className="rounded-full outline outline-4 outline-[#6AD26A]">
                         <Image
