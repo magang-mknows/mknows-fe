2906b67167bf1b384a44e30df92baa442986b0e4
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-11 15:19:26 +0700

    remove bg and text dark mode

--- apps/lxp/modules/guide-guide video/section/ListVideo.tsx
+++ apps/lxp/modules/guide-guide video/section/ListVideo.tsx
@@ -8,7 +8,7 @@ const ListVideo = (): ReactElement => {
   return (
     <div className="w-full">
       <div className="title mb-[2px]">
-        <h1 className="text-black dark:text-white font-[700] text-[20px]">
+        <h1 className="text-black font-[700] text-[20px]">
           Video panduan lainnya
         </h1>
       </div>
@@ -21,7 +21,7 @@ const ListVideo = (): ReactElement => {
             >
               <div
                 key={index}
-                className="w-full bg-white dark:bg-[#232529] h-fit lg:h-[120px] rounded-[8px] shadow-sm mt-[12px]"
+                className="w-full bg-white  h-fit lg:h-[120px] rounded-[8px] shadow-sm mt-[12px]"
               >
                 <div className="flex px-[10px] py-[10px] gap-[16px]">
                   <Image
@@ -32,10 +32,10 @@ const ListVideo = (): ReactElement => {
                     alt={'thumbnail'}
                   />
                   <div>
-                    <h1 className="text-[14px] font-[700] text-black dark:text-white">
+                    <h1 className="text-[14px] font-[700] text-black">
                       {item.titleVideo}
                     </h1>
-                    <p className="text-[12px] font-[400] text-black dark:text-white text-justify mt-[4px]">
+                    <p className="text-[12px] font-[400] text-black  text-justify mt-[4px]">
                       {item.descVideo?.substring(0, 120)} ...
                     </p>
                   </div>
