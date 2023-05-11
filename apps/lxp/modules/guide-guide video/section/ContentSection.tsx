0e4426b59013925f3e60308a5b9b2d1619eccb78
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-11 15:00:52 +0700

    migrate section content guide video to monorepo

---
+++ apps/lxp/modules/guide-guide video/section/ContentSection.tsx
@@ -0,0 +1,43 @@
+import React, { FC, ReactElement } from 'react';
+import { useRecoilValue } from 'recoil';
+import Description from './Description';
+import ListStudy from './ListVideo';
+import YoutubeSection from './YoutubeSection';
+import { filterOption } from '../store';
+import { useRouter } from 'next/router';
+
+const ContentSection: FC = (): ReactElement => {
+  const { query } = useRouter();
+  const getOption = useRecoilValue(
+    filterOption(query.videoId as unknown as string)
+  );
+  console.log(query);
+  return (
+    <div className="mx-6 md:mx-8 lg:px10 my-2  bg-[#FFFF] shadow-sm rounded-[8px]">
+      <h1 className="text-center font-[700] lg:text-[25px] text-[20px] pt-[50px]">
+        Pelatihan Keterampilan Komunikasi
+      </h1>
+      {getOption.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="lg:flex gap-[68px] xl:flex-nowrap lg:flex-wrap lg:p-[50px] p-5"
+          >
+            <div className="w-full">
+              <YoutubeSection videoId={item.videoId} />
+              <Description
+                titleVideo={item.titleVideo}
+                descVideo={item.descVideo}
+              />
+            </div>
+            <div className="w-full">
+              <ListStudy />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ContentSection;
