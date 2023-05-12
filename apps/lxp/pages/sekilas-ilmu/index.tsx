33a8ee7a697162e0a439f9ba44f0fc074ea394b5
Muhamad Fauzan <47935196+fauzannmw@users.noreply.github.com>
2023-05-12 14:04:29 +0700

    feature: done migrate glimpse and glimpse-layout module lxp

--- apps/lxp/pages/sekilas-ilmu/index.tsx
+++ apps/lxp/pages/sekilas-ilmu/index.tsx
@@ -1,9 +1,14 @@
 // import GlimpseKnowledgeModule from '@/modules/glimpse-knowledge';
+import { GlimpseKnowledgeModule } from '../../modules';
 import { NextPage } from 'next';
 import { ReactElement } from 'react';
 
 const index: NextPage = (): ReactElement => {
-  return <div>{/* <GlimpseKnowledgeModule /> */}</div>;
+  return (
+    <div>
+      <GlimpseKnowledgeModule />
+    </div>
+  );
 };
 
 export default index;
