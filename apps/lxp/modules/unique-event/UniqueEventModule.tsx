8b7bb0e7a834bcffdf3adf119b0aedaa5ebe1c9a
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-05-14 19:58:20 +0700

    bugfix:unique event image and all features page

--- apps/lxp/modules/unique-event/UniqueEventModule.tsx
+++ apps/lxp/modules/unique-event/UniqueEventModule.tsx
@@ -1,10 +1,9 @@
 import { useRouter } from 'next/router';
 import React, { FC, ReactElement } from 'react';
-import { UniqueEventBreadCumbs } from './constant';
+import { UniqueEventBreadCumbs } from './const';
 import { BreadCrumb } from '@mknows-frontend-services/components/atoms';
-import UniqueEventSidebar from './event-sidebar/UniqueEventSidebar';
-import UniqueEventSearch from './event-search/UniqueEventSearch';
-import UniqueEventContent from './event-content/UniqueEventContent';
+import { UniqueEventSidebar } from './event-sidebar/UniqueEventSidebar';
+import { UniqueEventContent } from './event-content';
 
 export const UniqueEventModule: FC = (): ReactElement => {
   const router = useRouter();
@@ -21,14 +20,11 @@ export const UniqueEventModule: FC = (): ReactElement => {
   }
 
   return (
-    <section className="bg-neutral-50/60 min-h-[100vh] pb-20">
-      <BreadCrumb items={UniqueEventBreadCumbs} />
+    <section className="bg-neutral-100/30 min-h-[100vh] pb-20">
+      <BreadCrumb items={UniqueEventBreadCumbs} textColor="text-version2-400" />
       <section className="grid grid-cols-1 gap-20 px-8 lg:grid-cols-3 md:px-14 lg:px-16">
         <UniqueEventSidebar />
-        <section className="col-span-2">
-          <UniqueEventSearch />
-          <UniqueEventContent />
-        </section>
+        <UniqueEventContent />
       </section>
     </section>
   );
