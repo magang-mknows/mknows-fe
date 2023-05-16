7c3eef3ca12ac4e2257182cd7fd7b98a927e1e5f
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-05-17 01:39:57 +0700

    feature: api setup for update profile and data

--- apps/lxp/modules/profile/main/edit-profile/hook.ts
+++ apps/lxp/modules/profile/main/edit-profile/hook.ts
@@ -1,7 +1,18 @@
-import { UseQueryResult, useQuery } from '@tanstack/react-query';
-import { TUserDataResponse } from './types';
+import {
+  UseMutationResult,
+  UseQueryResult,
+  useMutation,
+  useQuery,
+} from '@tanstack/react-query';
+import {
+  IUpdateUserPayload,
+  TUpdateUserResponse,
+  TUserDataResponse,
+  TUserProfilePayload,
+  TUserProfileResponse,
+} from './types';
 import { TMetaErrorResponse } from '@mknows-frontend-services/utils';
-import { getUserData } from './api';
+import { getUserData, updateUserData, updateUserProfile } from './api';
 
 export const useGetUserData = (): UseQueryResult<
   TUserDataResponse,
@@ -10,4 +21,30 @@ export const useGetUserData = (): UseQueryResult<
   useQuery({
     queryKey: ['get-user-data'],
     queryFn: async () => await getUserData(),
+  });
+
+export const useUpdateUserProfile = (): UseMutationResult<
+  TUserProfileResponse,
+  TMetaErrorResponse,
+  TUserProfilePayload,
+  unknown
+> =>
+  useMutation({
+    mutationKey: ['update-user-profile'],
+    mutationFn: async (payload) => {
+      return await updateUserProfile(payload);
+    },
+  });
+
+export const useUpdateUserData = (): UseMutationResult<
+  TUpdateUserResponse,
+  TMetaErrorResponse,
+  IUpdateUserPayload,
+  unknown
+> =>
+  useMutation({
+    mutationKey: ['update-user-data'],
+    mutationFn: async (payload) => {
+      return await updateUserData(payload);
+    },
   });
