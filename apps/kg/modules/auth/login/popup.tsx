edf0d5a945903c8327c591dc3d7f0b4dcf9b49c3
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-05-16 14:27:19 +0700

    feature: add popup in forgot pass

---
+++ apps/kg/modules/auth/login/popup.tsx
@@ -0,0 +1,67 @@
+import { PopupModal } from '@mknows-frontend-services/components/molecules';
+import { FC, ReactElement } from 'react';
+import { usePopupForgotPass } from './hook';
+import { Button, TextField } from '@mknows-frontend-services/components/atoms';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+export const PopupForgot: FC = (): ReactElement => {
+  const { setPopupStatus, getPopupStatus } = usePopupForgotPass();
+  const validationSchema = z.object({
+    email: z.string().min(1, { message: 'Email harus diisi' }).email({
+      message: 'Email harus valid',
+    }),
+  });
+
+  type ValidationSchema = z.infer<typeof validationSchema>;
+  const {
+    control,
+    formState: { isValid, errors },
+  } = useForm<ValidationSchema>({
+    resolver: zodResolver(validationSchema),
+    mode: 'all',
+    defaultValues: {
+      email: '',
+    },
+  });
+  return (
+    <PopupModal
+      onClose={() => setPopupStatus(false)}
+      lookup={getPopupStatus}
+      className="!w-[100%] text-md md:px-16 sm:px-14"
+      popupTitle="Lupa Kata Sandi"
+    >
+      <div className="mb-5 lg:text-[16px] md:text-[14px]">
+        Masukkan alamat email Anda yang terdaftar. Kami akan
+        <br />
+        mengirimkan kode OTP untuk dapat mengatur ulang kata sandi Anda.
+      </div>
+
+      <form className="flex flex-col w-full !justify-end">
+        <label className="text-start font-[500] text-[16px] mb-1">
+          Email
+          <TextField
+            type="email"
+            variant="lg"
+            name={'email'}
+            control={control}
+            placeholder="Masukan email"
+            status={errors.email ? 'error' : 'none'}
+            message={errors.email?.message}
+          />
+        </label>
+
+        <div className="flex justify-center text-center w-full">
+          <Button
+            type="submit"
+            disabled={!isValid}
+            className=" w-fit px-8 py-3 disabled:bg-neutral-400 h-auto text-[16px] text-white rounded-lg border-2 border-neutral-200 appearance-none bg-primary-600 font-[600] tracking-wide"
+          >
+            Kirim
+          </Button>
+        </div>
+      </form>
+    </PopupModal>
+  );
+};
