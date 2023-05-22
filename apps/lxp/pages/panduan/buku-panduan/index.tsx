5bd4aec0683c8c03cf0e19f2490488011089f906
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-22 13:58:07 +0700

    migrate pages guide book

---
+++ apps/lxp/pages/panduan/buku-panduan/index.tsx
@@ -0,0 +1,9 @@
+import { GuideBookModule } from '@lxp/modules/guide-guide book';
+import { NextPage } from 'next';
+import React, { ReactElement } from 'react';
+
+const page: NextPage = (): ReactElement => {
+  return <GuideBookModule />;
+};
+
+export default page;
