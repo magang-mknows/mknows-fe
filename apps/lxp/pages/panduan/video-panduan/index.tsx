2ba12cb5a0bae62157d4660df94ee7bfa271b4b1
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-22 13:59:34 +0700

    migrate pages guide video

---
+++ apps/lxp/pages/panduan/video-panduan/index.tsx
@@ -0,0 +1,9 @@
+import { GuideVideoHomeModule } from '../../../modules/guide-guide video home/module';
+import { NextPage } from 'next';
+import React, { ReactElement } from 'react';
+
+const page: NextPage = (): ReactElement => {
+  return <GuideVideoHomeModule />;
+};
+
+export default page;
