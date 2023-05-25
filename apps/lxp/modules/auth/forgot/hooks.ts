14a35c1f43cadf6e16b65c9dec25eff15820e571
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-26 00:13:31 +0700

    create custom hooks forgot pw

---
+++ apps/lxp/modules/auth/forgot/hooks.ts
@@ -0,0 +1,25 @@
+import { TMetaErrorResponse, TMetaItem } from "@mknows-frontend-services/utils";
+import { UseMutationResult, useMutation } from "@tanstack/react-query";
+import { TForgotPasswordPayload, TusePopupForgotPass } from "./types";
+import { forgotPasswordRequest } from "./api";
+import { useRecoilState } from "recoil";
+import { PopupModalForgotPass } from "./store";
+
+export const useForgot = (): UseMutationResult<
+  TMetaItem,
+  TMetaErrorResponse,
+  TForgotPasswordPayload,
+  unknown
+> =>
+  useMutation({
+    mutationKey: ["forgot-password-post"],
+    mutationFn: async (payload) => await forgotPasswordRequest(payload),
+  });
+
+export const usePopupForgotPass = (): TusePopupForgotPass => {
+  const [get, set] = useRecoilState(PopupModalForgotPass);
+  return {
+    setPopupStatus: (val: boolean) => set(val),
+    getPopupStatus: get,
+  };
+};
