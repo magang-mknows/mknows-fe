2572c62327021a08d26d2b20b301f81b597236e4
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-05-26 09:48:59 +0630

    improvement: integrate in popup leaderboard

--- apps/kg/modules/leaderboard/components/popup-profil.tsx
+++ apps/kg/modules/leaderboard/components/popup-profil.tsx
@@ -15,32 +15,32 @@ export const PopupProfil: FC<PopupProfilProps> = ({
   widthModal,
   ...props
 }): ReactElement => {
-  const data = [
+  const template = [
     {
       src: Like,
       title: "Jumlah like yang didapat",
-      desc: 100,
+      desc: `${props.author?.discussion_likes === null ? 0 : props.author?.discussion_likes}`,
       colorBg: "bg-[#CDF5FA]",
       colorText: "text-[#106FA4]",
     },
     {
       src: Chat,
       title: "Jumlah Diskusi",
-      desc: 50.0,
+      desc: `${props.author?.discussion_posted === null ? 0 : props.author?.discussion_posted}`,
       colorBg: "bg-[#FEF6D0]",
       colorText: "text-[#FAB317]",
     },
     {
       src: Clock,
       title: "IPK",
-      desc: "4.00",
+      desc: `${props.author?.ipk === null ? 0 : props.author?.ipk}`,
       colorBg: "bg-[#E3FBDA]",
       colorText: "text-[#3EB449]",
     },
     {
       src: Building,
       title: "Sertifikat",
-      desc: "10.000",
+      desc: `${props.author?.total_certificates === null ? 0 : props.author?.total_certificates}`,
       colorBg: "bg-[#FEDBD7]",
       colorText: "text-[#ED3768]",
     },
@@ -55,27 +55,33 @@ export const PopupProfil: FC<PopupProfilProps> = ({
         </h1>
         <div className="flex sm:flex-row flex-col outline sm:justify-between justify-start outline-[#E5E5E5] bg-[#FAFAFA] rounded-[8px] md:px-10 px-0 h-[125px] w-full items-center">
           <div className="flex items-center sm:gap-7 gap-4 sm:-mt-3">
-            <Image src={Avatar} alt={"avatar"} className="w-24 h-24 mt-4" />
+            <Image
+              src={props.author?.avatar === null ? Avatar : props.author?.avatar}
+              width={24}
+              height={24}
+              alt={"avatar"}
+              className="w-24 h-24 mt-4"
+            />
             <div className="sm:mt-0 -mt-5">
               <p className="text-[#171717] font-[600] sm:text-[24px] text-[20px] text-left">
                 {props.student_name}
               </p>
               <div className="flex flex-row text-[#737373] sm:text-[20px] text-[16px] font-[500] gap-2">
-                <p>Teknik</p>
+                <p>{props.author?.major}</p>
                 <Image src={Circle} alt={"circle"} />
-                <p>Semester 1</p>
+                <p>2</p>
               </div>
             </div>
           </div>
           <div className="flex item-center sm:ml-0 ml-8 sm:mt-0 -mt-8">
             <button className="bg-[#FAB317] text-white sm:text-[16px] text-[14px] font-[600] rounded-[8px] px-2 py-1 ">
-              {props.averageScore} Poin
+              {props.averageScore == null ? 0 : props.averageScore} Poin
             </button>
           </div>
         </div>
         <div className="content w-full mt-5">
           <div className="flex flex-row w-full justify-around flex-wrap gap-5">
-            {data.map((items, i) => (
+            {template.map((items, i) => (
               <div
                 key={i}
                 className={`px-3 py-3 ${items.colorBg}  rounded-[8px] lg:w-[340px] w-full h-[101px] flex flex-row items-center gap-3 `}
