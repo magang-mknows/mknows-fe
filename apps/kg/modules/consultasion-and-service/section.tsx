c86199ebd14406294655238d71896e79a2662a05
Fenny Oktaviani <fnny04@gmail.com>
2023-05-26 16:31:55 +0700

    improvement : add card

--- apps/kg/modules/consultasion-and-service/section.tsx
+++ apps/kg/modules/consultasion-and-service/section.tsx
@@ -1,5 +1,12 @@
 import { FC, ReactElement } from "react";
 
 export const ConsultationAndServiceModule: FC = (): ReactElement => {
-  return <h1>hallo</h1>;
+  return (
+    <main className="bg-neutral-100 w-full h-screen">
+      <div className="flex flex-col bg-primary-500 h-[244px] mx-[72px] rounded-md justify-center items-center text-white gap-[14px]">
+        <h1 className="font-bold text-[36px]">Konsultasi & Layanan</h1>
+        <p className="text-[28px]">Apa yang bisa kami bantu untuk Anda?</p>
+      </div>
+    </main>
+  );
 };
