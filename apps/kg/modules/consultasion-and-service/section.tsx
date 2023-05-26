e92ed69a586b137b7dc7938a0312d211d20950e7
Fenny Oktaviani <fnny04@gmail.com>
2023-05-26 16:43:27 +0700

    improvement : add random icon ?

--- apps/kg/modules/consultasion-and-service/section.tsx
+++ apps/kg/modules/consultasion-and-service/section.tsx
@@ -1,4 +1,6 @@
 import { FC, ReactElement } from "react";
+import Image from "next/image";
+import random from "./icon/random.svg";
 
 export const ConsultationAndServiceModule: FC = (): ReactElement => {
   return (
@@ -6,6 +8,9 @@ export const ConsultationAndServiceModule: FC = (): ReactElement => {
       <div className="flex flex-col bg-primary-500 h-[244px] mx-[72px] rounded-md justify-center items-center text-white gap-[14px]">
         <h1 className="font-bold text-[36px]">Konsultasi & Layanan</h1>
         <p className="text-[28px]">Apa yang bisa kami bantu untuk Anda?</p>
+        <div className="flex absolute pt-44 pl-16">
+          <Image src={random} width={20} alt="?" />
+        </div>
       </div>
     </main>
   );
