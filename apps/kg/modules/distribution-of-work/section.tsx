4cc130899d4b04083770a58afc77f391b9d3e192
Fenny Oktaviani <fnny04@gmail.com>
2023-05-27 00:08:41 +0700

    improvement : add breadcrumb,main section,and title

--- apps/kg/modules/distribution-of-work/section.tsx
+++ apps/kg/modules/distribution-of-work/section.tsx
@@ -1,5 +1,11 @@
 import { FC, ReactElement } from "react";
 
 export const DistributionOfWorkModule: FC = (): ReactElement => {
-  return <h1>hallo</h1>;
+  return (
+    <main className="bg-neutral-100 h-screen px-[66px] py-[30px]">
+      <div>
+        <h1 className="text-[24px] font-bold">Penyaluran Kerja</h1>
+      </div>
+    </main>
+  );
 };
--- apps/kg/pages/penyaluran-kerja/index.tsx
+++ apps/kg/pages/penyaluran-kerja/index.tsx
@@ -1,9 +1,17 @@
 import { NextPage } from "next";
 import { ReactElement } from "react";
 import { DistributionOfWorkModule } from "../../modules";
+import { BaseLayout } from "../../modules";
+import { BreadCrumb } from "@mknows-frontend-services/components/atoms";
+import { distributionService } from "../../modules";
 
 const DistributionOfWorkPage: NextPage = (): ReactElement => {
-  return <DistributionOfWorkModule />;
+  return (
+    <BaseLayout>
+      <BreadCrumb items={distributionService} />
+      <DistributionOfWorkModule />
+    </BaseLayout>
+  );
 };
 
 export default DistributionOfWorkPage;
