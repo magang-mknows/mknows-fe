e19ef239b53fdffd3d717438e86d5f43b95ec029
Fenny Oktaviani <fnny04@gmail.com>
2023-05-27 00:52:05 +0700

    improvement : create mitra card

--- apps/kg/modules/distribution-of-work/card/mitra2.tsx
+++ apps/kg/modules/distribution-of-work/card/mitra2.tsx
@@ -1,18 +1,18 @@
 import { FC, ReactElement } from "react";
 import Image from "next/image";
-import mayora from "./assets/mayora.svg";
+import gojek from "./assets/gojek.svg";
 
-export const Mitra1: FC = (): ReactElement => {
+export const Mitra2: FC = (): ReactElement => {
   return (
     <div className="flex flex-col bg-white w-[280px] h-[350px] rounded-md my-6 shadow-xl px-4">
       <div className="flex w-full justify-center">
-        <Image src={mayora} alt="logo" />
+        <Image src={gojek} alt="logo" />
       </div>
-      <h1 className="text-primary-500 font-bold">Design Grafis</h1>
-      <p className="text-primary-500 py-4">PT Mayora Indonesia</p>
+      <h1 className="text-primary-500 font-bold">FronEnd Dev</h1>
+      <p className="text-primary-500 py-4">PT Gojek Indonesia</p>
       <p className="text-neutral-500 ">Kota Depok Jawa Barat</p>
-      <p className="text-neutral-500">Rp 3.500.000 - 5.000.000</p>
-      <p className="text-neutral-500">Full Time</p>
+      <p className="text-neutral-500">Rp 2.000.000 - 6.000.000</p>
+      <p className="text-neutral-500">Freelance</p>
       <div className="bg-primary-100 my-4 text-primary-500 border border-primary-500 p-2 text-sm rounded-md">
         Terbuka hingga 26 Juni 2024
       </div>
