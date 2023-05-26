a3c45cbed0800f4a9031df1a9a519994f6a8e8d0
Fenny Oktaviani <fnny04@gmail.com>
2023-05-27 00:47:07 +0700

    improvement : add description mitra

--- apps/kg/modules/distribution-of-work/module.tsx
+++ apps/kg/modules/distribution-of-work/module.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactElement } from "react";
 import Image from "next/image";
 import search from "./assets/search.svg";
+import mayora from "./assets/mayora.svg";
 
 export const DistributionOfWorkModule: FC = (): ReactElement => {
   return (
@@ -121,7 +122,19 @@ export const DistributionOfWorkModule: FC = (): ReactElement => {
           </div>
         </div>
         <div className="grid grid-cols-3 gap-[28px]">
-          <div className="bg-white w-[280px] h-[350px] rounded-md my-6 shadow-xl"></div>
+          <div className="flex flex-col bg-white w-[280px] h-[350px] rounded-md my-6 shadow-xl px-4">
+            <div className="flex w-full justify-center">
+              <Image src={mayora} alt="logo" />
+            </div>
+            <h1 className="text-primary-500 font-bold">Design Grafis</h1>
+            <p className="text-primary-500 py-4">PT Mayora Indonesia</p>
+            <p className="text-neutral-500 ">Kota Depok Jawa Barat</p>
+            <p className="text-neutral-500">Rp 3.500.000 - 5.000.000</p>
+            <p className="text-neutral-500">Full Time</p>
+            <div className="bg-primary-100 my-4 text-primary-500 border border-primary-500 p-2 text-sm rounded-md">
+              Terbuka hingga 26 Juni 2024
+            </div>
+          </div>
           <div className="bg-white w-[280px] h-[350px] rounded-md my-6 shadow-xl"></div>
           <div className="bg-white w-[280px] h-[350px] rounded-md my-6 shadow-xl"></div>
         </div>
