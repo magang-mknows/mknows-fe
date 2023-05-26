545f407d5545b05899a435226f3b879fe026644f
Fenny Oktaviani <fnny04@gmail.com>
2023-05-26 23:19:11 +0700

    improvement : add new section

--- apps/kg/modules/consultasion-and-service/section.tsx
+++ apps/kg/modules/consultasion-and-service/section.tsx
@@ -69,6 +69,10 @@ export const ConsultationAndServiceModule: FC = (): ReactElement => {
           </div>
         </div>
       </section>
+      <section className="mx-[136px]">
+        <h1 className="text-[28px] font-semibold">Petanyaan yang sering Ditanyakan</h1>
+        <p>Berikut list pertanyaan-pertanyaan yang sering ditanyakan kepada kami </p>
+      </section>
     </main>
   );
 };
