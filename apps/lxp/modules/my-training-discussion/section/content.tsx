31723254605fc30de19fd8b95ab4e1c94413f0d7
Muhamad Fauzan <47935196+fauzannmw@users.noreply.github.com>
2023-05-29 15:52:37 +0700

    feature: integrating getAllDiscussion mytraining lxp

--- apps/lxp/modules/my-training-discussion/section/content.tsx
+++ apps/lxp/modules/my-training-discussion/section/content.tsx
@@ -13,51 +13,54 @@ const Content: FC = (): ReactElement => {
 
   return (
     <>
-    {listDiscussionData?.map}
-      <section>
-        <CardAuthor
-          title={"Manajemen Keuangan"}
-          author={"Bakti Husada"}
-          avatar={profileDummy}
-          time={"20 menit"}
-          content={"halo"}
-          role={"mahasiswa"}
-        />
-        {/* reply */}
-        <div className="flex flex-row gap-4">
-          <p className="text-[#106FA4] font-[600] text-[16px]">3 Balasan</p>
-          <Image src={Down} className="w-auto" width={100} height={100} alt={"down"} />
-        </div>
+      {listDiscussionData?.map((discussion, index) => {
+        return (
+          <section key={index}>
+            <CardAuthor
+              title={discussion.title}
+              author={"Bakti Husada"}
+              avatar={profileDummy}
+              time={"20 menit"}
+              content={discussion.content}
+              role={"mahasiswa"}
+            />
+            {/* reply */}
+            <div className="flex flex-row gap-4">
+              <p className="text-[#106FA4] font-[600] text-[16px]">3 Balasan</p>
+              <Image src={Down} className="w-auto" width={100} height={100} alt={"down"} />
+            </div>
 
-        <CardComment
-          styleCard="md:ml-14 ml-9 bg-[#F5F5F5]"
-          title={"Manajemen Keuangan"}
-          author={"Bakti Husada"}
-          avatar={profileDummy}
-          time={"20 menit"}
-          content={
-            "Diskusikan tentang manajemen keuangan menurut pemahaman anda? Apakah fungsi dan tujuan dari manajemen keuangan. Apa modul yang telah kalian pelajari sudah jelas"
-          }
-          role={"mahasiswa"}
-        />
-        {/* reply */}
-        <div className="flex flex-row gap-4 ml-14">
-          <p className="text-[#106FA4] font-[600] text-[16px]">3 Balasan</p>
-          <Image src={Down} className="w-auto" width={100} height={100} alt={"down"} />
-        </div>
+            <CardComment
+              styleCard="md:ml-14 ml-9 bg-[#F5F5F5]"
+              title={"Manajemen Keuangan"}
+              author={"Bakti Husada"}
+              avatar={profileDummy}
+              time={"20 menit"}
+              content={
+                "Diskusikan tentang manajemen keuangan menurut pemahaman anda? Apakah fungsi dan tujuan dari manajemen keuangan. Apa modul yang telah kalian pelajari sudah jelas"
+              }
+              role={"mahasiswa"}
+            />
+            {/* reply */}
+            <div className="flex flex-row gap-4 ml-14">
+              <p className="text-[#106FA4] font-[600] text-[16px]">3 Balasan</p>
+              <Image src={Down} className="w-auto" width={100} height={100} alt={"down"} />
+            </div>
 
-        <CardComment
-          styleCard="md:ml-28 ml-23 bg-[#F5F5F5]"
-          title={"Manajemen Keuangan"}
-          author={"Bakti Husada"}
-          avatar={profileDummy}
-          time={"20 menit"}
-          content={
-            "Diskusikan tentang manajemen keuangan menurut pemahaman anda? Apakah fungsi dan tujuan dari manajemen keuangan. Apa modul yang telah kalian pelajari sudah jelas"
-          }
-          role={"mahasiswa"}
-        />
-      </section>
+            <CardComment
+              styleCard="md:ml-28 ml-23 bg-[#F5F5F5]"
+              title={"Manajemen Keuangan"}
+              author={"Bakti Husada"}
+              avatar={profileDummy}
+              time={"20 menit"}
+              content={
+                "Diskusikan tentang manajemen keuangan menurut pemahaman anda? Apakah fungsi dan tujuan dari manajemen keuangan. Apa modul yang telah kalian pelajari sudah jelas"
+              }
+              role={"mahasiswa"}
+            />
+          </section>
+        );
+      })}
 
       {/* redo*/}
       <div className="flex flex-row gap-4 ml-28">
