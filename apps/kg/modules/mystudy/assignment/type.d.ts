92d50c48dda13a10cbe8e3a3b8d7279a114ec50b
Nandi Irawan <nandiirawan327@gmail.com>
2023-05-31 02:51:06 +0700

    improvement: change all studi-ku api middleware path

--- apps/kg/modules/mystudy/assignment/type.d.ts
+++ apps/kg/modules/mystudy/assignment/type.d.ts
@@ -1,3 +1,5 @@
+import { TMetaResponseSingle } from "@mknows-frontend-services/utils";
+
 export type Instruction = {
   matkul: string;
   pertemuan: number;
@@ -6,3 +8,47 @@ export type Instruction = {
   file: string;
   deadline: string;
 };
+
+// Service API
+
+export type TAssignmentItem = {
+  assignment: TAssignment;
+  student_progress: TStudentProgress;
+};
+
+type TAssignment = {
+  id: string;
+  documents: string[];
+  title: string;
+  desc: string;
+  duration_days: number;
+  session_assignment: TSessionAssignment;
+};
+
+type TSessionAssignment = {
+  session_no: number;
+  subject_session: TSubjectSession;
+};
+
+type TSubjectSession = {
+  id: string;
+  name: string;
+  teacher_subject: TTeacherSubject;
+};
+
+type TTeacherSubject = {
+  full_name: string;
+};
+
+type TStudentProgress = {
+  status: string;
+  type: string;
+  score: any;
+  is_late: boolean;
+  assignment_answer: string[];
+  timestamp_taken: string;
+  timestamp_submitted: string;
+  deadline: string;
+};
+
+export type TAssignmentResponse = TMetaResponseSingle<TAssignmentItem>;
--- apps/kg/pages/api/studi-ku/[detail-matkul]/kuis/[quizId]/skor/[quizScoreId]/index.tsx
+++ apps/kg/pages/api/studi-ku/[detail-matkul]/kuis/[quizId]/skor/[quizScoreId]/index.tsx
@@ -1,16 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const STATUS_CODE = 200;
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
-  if (req.method == 'GET') {
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  if (req.method == "GET") {
     res.status(STATUS_CODE).json({
       code: 200,
-      status: 'SUCCESS Test',
-      message: 'Success submit test quiz',
+      status: "SUCCESS Test",
+      message: "Success submit test quiz",
       data: {
         right: 8,
         wrong: 2,
