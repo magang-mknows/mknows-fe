9dd10dbb73fdb6bb05d5a94d779908ddc1f596b8
Fenny Oktaviani <fnny04@gmail.com>
2023-05-30 21:44:54 +0700

    improvement : add api for identity request

--- apps/cs/src/modules/user/add-data/api.ts
+++ apps/cs/src/modules/user/add-data/api.ts
@@ -3,6 +3,13 @@ import { TIdentityFile, TIdentityResponse } from "./types";
 import { serialize } from "object-to-formdata";
 
 export const fileIdentityRequest = async (payload: TIdentityFile): Promise<TIdentityResponse> => {
-  const { data } = await api.post("/user/identity-scoring", serialize(payload));
+  const { data } = await api({
+    method: "post",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    url: "/user/identity-scoring",
+    data: serialize(payload),
+  });
   return data;
 };
