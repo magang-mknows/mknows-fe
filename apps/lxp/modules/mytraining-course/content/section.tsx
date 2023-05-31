b4209f2f4d6a46f8a7d8400d31ed4e7823e3ad8a
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-31 16:06:32 +0700

    integrating api my-training course content

--- apps/lxp/modules/mytraining-course/content/section.tsx
+++ apps/lxp/modules/mytraining-course/content/section.tsx
@@ -4,41 +4,87 @@ import { HiDocumentText, HiOutlineChatBubbleBottomCenterText } from "react-icons
 import { RiTodoLine } from "react-icons/ri";
 import { Disclosure } from "@headlessui/react";
 import { PanelSection } from "./panel";
+import { useRouter } from "next/router";
+import { useGetMyWorkCourse } from "../hooks";
 
 export const ContentSection: FC = (): ReactElement => {
+  const { query } = useRouter();
+  const { data } = useGetMyWorkCourse(
+    query.subjectID as string,
+    query.lastSession as string,
+    query.batchID as string,
+  );
+  console.log("data", data);
   return (
     <div className="gap-[30px] min-h-[60vh] px-8 md:px-14 lg:px-16 mt-[25px]">
-      <Disclosure as={"div"} className={"flex flex-col"}>
-        <Disclosure.Button
-          className={`px-[30px] py-[14px] flex justify-between items-center rounded-lg text-white cursor-pointer bg-[#053D38]`}
-        >
-          <p className="text-base text-neutral-50 font-medium">Pertemuan 1</p>
+      {data?.data?.dataSessions.map((item: any, index: number) => {
+        return (
+          <Disclosure as={"div"} className={"flex flex-col mb-5"} key={index}>
+            <Disclosure.Button
+              className={`px-[30px] py-[14px] flex justify-between items-center rounded-lg text-white  ${
+                item.is_locked ? " bg-[#A3A3A3]" : "bg-[#053D38] cursor-pointer"
+              }`}
+              disabled={item.is_locked}
+            >
+              <p className="text-base text-neutral-50 font-medium">Pertemuan {index + 1}</p>
 
-          <MdArrowDropDown className={`text-2xl text-neutral-50`} />
-        </Disclosure.Button>
-        <Disclosure.Panel className={"w-[98%] mx-auto bg-neutral-50"}>
-          <PanelSection
-            title="Modul"
-            href="/pelatihanku/modul/test"
-            icon={<HiDocumentText className="text-xl text-[#40A0C8]" />}
-          />
-          <PanelSection
-            title="Quiz"
-            href="/pelatihanku/quiz"
-            icon={<MdOutlineQuiz className="text-xl text-[#FAB317]" />}
-          />
-          <PanelSection
-            title="Tugas"
-            href="/penugasan"
-            icon={<RiTodoLine className="text-xl text-[#F4677F]" />}
-          />
-          <PanelSection
-            title="Diskusi"
-            href="/pelatihanku/diskusi"
-            icon={<HiOutlineChatBubbleBottomCenterText className="text-xl text-[#3EB449]" />}
-          />
-        </Disclosure.Panel>
-      </Disclosure>
+              <MdArrowDropDown className={`text-2xl text-neutral-50`} />
+            </Disclosure.Button>
+
+            <Disclosure.Panel className={"w-[98%] mx-auto bg-neutral-50"} key={index}>
+              {item.progress.reverse().map((item: any, index: number) => {
+                return (
+                  <>
+                    <PanelSection
+                      key={index}
+                      title={
+                        item.type == "MODULE"
+                          ? "Modul"
+                          : item.type == "ASSIGNMENT"
+                          ? "Penugasan"
+                          : ""
+                      }
+                      href={
+                        item.type == "MODULE"
+                          ? `/pelatihanku/modul/${item.id}/${query.batchID}`
+                          : item.type == "ASSIGNMENT"
+                          ? "Penugasan"
+                          : ""
+                      }
+                      icon={
+                        item.type == "MODULE" ? (
+                          <HiDocumentText className="text-xl text-[#40A0C8]" />
+                        ) : item.type == "ASSIGNMENT" ? (
+                          <RiTodoLine className="text-xl text-[#F4677F]" />
+                        ) : (
+                          ""
+                        )
+                      }
+                    />
+                    {/* <PanelSection
+                      title="Quiz"
+                      href={`/pelatihanku/quiz/${item.id}/${query.batchID}`}
+                      icon={<MdOutlineQuiz className="text-xl text-[#FAB317]" />}
+                    />
+                    <PanelSection
+                      title="Tugas"
+                      href="/penugasan"
+                      icon={<RiTodoLine className="text-xl text-[#F4677F]" />}
+                    />
+                    <PanelSection
+                      title="Diskusi"
+                      href="/pelatihanku/diskusi"
+                      icon={
+                        <HiOutlineChatBubbleBottomCenterText className="text-xl text-[#3EB449]" />
+                      }
+                    /> */}
+                  </>
+                );
+              })}
+            </Disclosure.Panel>
+          </Disclosure>
+        );
+      })}
     </div>
   );
 };
