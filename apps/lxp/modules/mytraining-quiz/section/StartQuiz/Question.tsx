7961352564aa9d8caf8d1eeac8c88bd432c02fa4
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-05-31 22:06:27 +0700

    feature : show total number

--- apps/lxp/modules/mytraining-quiz/section/StartQuiz/Question.tsx
+++ apps/lxp/modules/mytraining-quiz/section/StartQuiz/Question.tsx
@@ -82,85 +82,107 @@ export const Question = () => {
   const [getCurrentNumber, setCurrentNumber] = useRecoilState(currentQuestionState);
 
   return (
-    <div className=" border-2 rounded-md w-full border-neutral-100  py-10 px-8">
-      {questions.map((question, index) => {
-        return (
-          <div key={index} className={`${index + 1 === getCurrentNumber ? "block" : "hidden"}`}>
-            <div className="flex mb-6">
-              <h1 className="text-neutral-800 text-center font-bold text-base">
-                {index + 1}. {question.question}
-              </h1>
-            </div>
-            {question.answers.map((answer, index) => {
-              return (
-                <label
-                  key={index}
-                  className="relative mb-5 min-w-[600px] py-3 border-version3-300 border-2 text-sm rounded-lg text-version3-300 cursor-pointer hover:bg-neutral-100 transition-all ease-in-out duration-75 hover:font-bold block "
-                  htmlFor={answer.id}
-                  id={answer.id}
-                >
-                  <h1 className="text-center">
-                    {String.fromCharCode(96 + (index + 1)).toUpperCase()}. {answer.answer}
-                  </h1>
-                  <input
-                    value={answer.id}
+    <div className="grid grid-cols-3 p-8  lg:gap-[52px]">
+      <div className=" col-span-2 border-2 rounded-md w-full border-neutral-100  py-10 px-8">
+        {questions.map((question, index) => {
+          return (
+            <div key={index} className={`${index + 1 === getCurrentNumber ? "block" : "hidden"}`}>
+              <div className="flex mb-6">
+                <h1 className="text-neutral-800 text-center font-bold text-base">
+                  {index + 1}. {question.question}
+                </h1>
+              </div>
+              {question.answers.map((answer, index) => {
+                return (
+                  <label
+                    key={index}
+                    className="relative mb-5  py-3 border-version3-300 border-2 text-sm rounded-lg text-version3-300 cursor-pointer hover:bg-neutral-100 transition-all ease-in-out duration-75 hover:font-bold block "
+                    htmlFor={answer.id}
                     id={answer.id}
-                    type="radio"
-                    name="choice"
-                    className=" h-full w-full peer absolute top-0 cursor-pointer"
-                  />
-                </label>
-              );
-            })}
-          </div>
-        );
-      })}
+                  >
+                    <h1 className="text-center">
+                      {String.fromCharCode(96 + (index + 1)).toUpperCase()}. {answer.answer}
+                    </h1>
+                    <input
+                      value={answer.id}
+                      id={answer.id}
+                      type="radio"
+                      name="choice"
+                      className=" h-full w-full peer absolute appearance-none top-0 cursor-pointer"
+                    />
+                  </label>
+                );
+              })}
+            </div>
+          );
+        })}
 
-      <div className="flex gap-4 mt-12">
-        <Button
-          disabled={getCurrentNumber === 1}
-          type="button"
-          onClick={() => {
-            setCurrentNumber(getCurrentNumber - 1);
-          }}
-          className="flex gap-2 w-full hover:bg-neutral-100/50 items-center px-4 py-3 justify-center bg-neutral-50 border-2 border-version3-500 rounded-md shadow-sm disabled:bg-neutral-200 disabled:border-neutral-200 group "
-        >
-          <MdOutlineNavigateNext className="rotate-180 group-disabled:text-neutral-600 text-version3-500" />
-          <h1 className="text-version3-500 text-sm font-bold group-disabled:text-neutral-500">
-            Sebelumnya
-          </h1>
-        </Button>
-        <Button
-          type="button"
-          className="flex gap-2 w-full  items-center px-4 py-3 justify-center bg-[#FAB317] border-2 border-[#FAB317] rounded-md shadow-sm hover:bg-[#e1a015]"
-        >
-          <AiOutlineQuestionCircle className=" text-neutral-50" />
-          <h1 className="text-neutral-50 text-sm font-bold">Ragu-ragu</h1>
-        </Button>
+        <div className="flex gap-4 mt-12">
+          <Button
+            disabled={getCurrentNumber === 1}
+            type="button"
+            onClick={() => {
+              setCurrentNumber(getCurrentNumber - 1);
+            }}
+            className="flex gap-2 w-full hover:bg-neutral-100/50 items-center px-4 py-3 justify-center bg-neutral-50 border-2 border-version3-500 rounded-md shadow-sm disabled:bg-neutral-200 disabled:border-neutral-200 group "
+          >
+            <MdOutlineNavigateNext className="rotate-180 group-disabled:text-neutral-600 text-version3-500" />
+            <h1 className="text-version3-500 text-sm font-bold group-disabled:text-neutral-500">
+              Sebelumnya
+            </h1>
+          </Button>
+          <Button
+            type="button"
+            className="flex gap-2 w-full  items-center px-4 py-3 justify-center bg-[#FAB317] border-2 border-[#FAB317] rounded-md shadow-sm hover:bg-[#e1a015]"
+          >
+            <AiOutlineQuestionCircle className=" text-neutral-50" />
+            <h1 className="text-neutral-50 text-sm font-bold">Ragu-ragu</h1>
+          </Button>
 
-        {getCurrentNumber !== questions.length ? (
-          <Button
-            onClick={() => {
-              setCurrentNumber(getCurrentNumber + 1);
-            }}
-            type="button"
-            className={`flex gap-2  w-full items-center px-4 py-3 justify-center bg-version3-500 border-2 border-version3-500 rounded-md shadow-sm hover:bg-version2-500`}
-          >
-            <h1 className="text-neutral-50 text-sm font-bold">Selanjutnya</h1>
-            <MdOutlineNavigateNext className=" text-neutral-50" />
-          </Button>
-        ) : (
-          <Button
-            onClick={() => {
-              console.log("habis");
-            }}
-            type="button"
-            className={`flex gap-2 w-full  items-center px-4 py-3 justify-center bg-warning-500 border-2 border-warning-500 rounded-md shadow-sm hover:bg-warning-600`}
-          >
-            <h1 className="text-neutral-50 text-sm font-bold">Kumpulkan</h1>
-            <MdOutlineNavigateNext className=" text-neutral-50" />
-          </Button>
-        )}
+          {getCurrentNumber !== questions.length ? (
+            <Button
+              onClick={() => {
+                setCurrentNumber(getCurrentNumber + 1);
+              }}
+              type="button"
+              className={`flex gap-2  w-full items-center px-4 py-3 justify-center bg-version3-500 border-2 border-version3-500 rounded-md shadow-sm hover:bg-version2-500`}
+            >
+              <h1 className="text-neutral-50 text-sm font-bold">Selanjutnya</h1>
+              <MdOutlineNavigateNext className=" text-neutral-50" />
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
+                console.log("habis");
+              }}
+              type="button"
+              className={`flex gap-2 w-full  items-center px-4 py-3 justify-center bg-warning-500 border-2 border-warning-500 rounded-md shadow-sm hover:bg-warning-600`}
+            >
+              <h1 className="text-neutral-50 text-sm font-bold">Kumpulkan</h1>
+              <MdOutlineNavigateNext className=" text-neutral-50" />
+            </Button>
+          )}
+        </div>
+      </div>
+      <div className=" border-2 rounded-md h-fit border-neutral-100 py-10 px-8">
+        <h1 className="font-[700] text-base mb-6">Daftar Soal :</h1>
+        <section className="flex flex-wrap gap-3">
+          {[...Array(questions.length)].map((item, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => {
+                  setCurrentNumber(index + 1);
+                }}
+                className={`${
+                  index + 1 === (getCurrentNumber as number) ? "bg-neutral-200" : "bg-neutral-50"
+                } hover:bg-neutral-100 cursor-pointer h-10 w-10 rounded-md border-[1px] border-version2-400 grid place-items-center`}
+              >
+                {index + 1}
+              </div>
+            );
+          })}
+        </section>
       </div>
     </div>
   );
--- apps/lxp/modules/mytraining-quiz/section/StartQuiz/module.tsx
+++ apps/lxp/modules/mytraining-quiz/section/StartQuiz/module.tsx
@@ -1,5 +1,4 @@
 import { Question } from "./Question";
-import Count from "./Count";
 import { BreadCrumb } from "@mknows-frontend-services/components/atoms";
 import { myTrainingQuizBreadCumbs } from "../../constant";
 import { ClientProvider } from "../../../common/provider";
@@ -9,10 +8,7 @@ export const QuizStartPage = () => {
     <ClientProvider>
       <BreadCrumb items={myTrainingQuizBreadCumbs} textColor="text-version2-400" />
       <div className="mx-8 md:mx-14 lg:mx-16 bg-neutral-50 mt-4 rounded-md shadow-sm">
-        <div className="flex p-8  lg:gap-[52px]">
-          <Question />
-          <Count />
-        </div>
+        <Question />
       </div>
     </ClientProvider>
   );
