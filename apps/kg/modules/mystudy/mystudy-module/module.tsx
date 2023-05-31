9ab0de08c320d83fb2226d07eaaeb55021b2bce7
Nandi Irawan <nandiirawan327@gmail.com>
2023-06-01 01:11:36 +0700

    feature: add slicing design when dataMajor and dataSubject is empty

--- apps/kg/modules/mystudy/mystudy-module/module.tsx
+++ apps/kg/modules/mystudy/mystudy-module/module.tsx
@@ -1,8 +1,10 @@
-import { FC, ReactElement } from "react";
+import { FC, Fragment, ReactElement } from "react";
 import { useGetMyStudy } from "./hooks";
 import { MajorSection } from "./major";
 import { TDataMajor, TDataSubject } from "./types";
 import { SubjectSection } from "./subject";
+import Image from "next/image";
+import OrangBingungIcon from "./assets/orang-bingung.svg";
 
 export const MyStudyModule: FC = (): ReactElement => {
   const { data } = useGetMyStudy();
@@ -11,8 +13,17 @@ export const MyStudyModule: FC = (): ReactElement => {
 
   return (
     <div className="w-full">
-      <MajorSection {...(dataMajorProps as TDataMajor)} />
-      <SubjectSection dataSubjectProps={dataSubjectProps as Array<TDataSubject>} />
+      {dataMajorProps && dataSubjectProps && dataSubjectProps.length > 0 ? (
+        <Fragment>
+          <MajorSection {...(dataMajorProps as TDataMajor)} />
+          <SubjectSection dataSubjectProps={dataSubjectProps as Array<TDataSubject>} />
+        </Fragment>
+      ) : (
+        <div className="w-fit m-auto">
+          <Image src={OrangBingungIcon} alt="simulasi-null" />
+          <h1 className="text-xl font-bold text-center">Tidak Ada Data Mata Kuliah</h1>
+        </div>
+      )}
     </div>
   );
 };
