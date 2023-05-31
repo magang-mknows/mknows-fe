078b9b9cdc13c8bec983e51037ff11df4dfa6b8a
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-05-31 14:13:28 +0700

    improvment : quiz ui

--- apps/lxp/modules/mytraining-quiz/section/StartQuiz/Question.tsx
+++ apps/lxp/modules/mytraining-quiz/section/StartQuiz/Question.tsx
@@ -1,76 +1,51 @@
-import React from "react";
+import { Button } from "@mknows-frontend-services/components/atoms";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { MdOutlineNavigateNext } from "react-icons/md";
 
-const Question = () => {
+export const Question = () => {
   return (
-    <div className=" border-2 rounded-md w-full border-[#E5E5E5] p-[40px]">
-      <div className="flex justify-center">
-        <h1 className="text-[#000] text-center font-[600] text-[20px]">
+    <div className=" border-2 rounded-md w-full border-neutral-100  py-10 px-8">
+      <div className="flex mb-6">
+        <h1 className="text-neutral-800 text-center font-bold text-base">
           1. Berapakah harga yang diharuskan saat 10% bunga dari bank ?
         </h1>
       </div>
-      <div className="mt-[69px]">
-        <div className="flex justify-center mt-[30px]">
-          <div className="relative w-[70%] py-4 border-[#003A63] border-2 text-base rounded-lg text-[#003A63]">
-            <label>
-              <h1 className="text-center">a. Rp 70.000</h1>
-            </label>
-            <input
-              type="radio"
-              name="choice"
-              className="absolute top-0 left-0 w-full h-full appearance-none cursor-pointer"
-            />
-          </div>
-        </div>
-        <div className="flex justify-center mt-[30px]">
-          <div className="relative w-[70%] py-4 border-[#003A63] border-2 text-base rounded-lg text-[#003A63]">
-            <label>
-              <h1 className="text-center">b. Rp 70.000</h1>
-            </label>
-            <input
-              type="radio"
-              name="choice"
-              className="absolute top-0 left-0 w-full h-full appearance-none cursor-pointer"
-            />
-          </div>
-        </div>
-        <div className="flex justify-center mt-[30px]">
-          <div className="relative w-[70%] py-4 border-[#003A63] border-2 text-base rounded-lg text-[#003A63]">
-            <label>
-              <h1 className="text-center">c. Rp 70.000</h1>
-            </label>
-            <input
-              type="radio"
-              name="choice"
-              className="absolute top-0 left-0 w-full h-full appearance-none cursor-pointer"
-            />
-          </div>
-        </div>
-        <div className="flex justify-center mt-[30px]">
-          <div className="relative w-[70%] py-4 border-[#003A63] border-2 text-base rounded-lg text-[#003A63]">
-            <label>
-              <h1 className="text-center">d. Rp 70.000</h1>
-            </label>
-            <input
-              type="radio"
-              name="choice"
-              className="absolute top-0 left-0 w-full h-full appearance-none cursor-pointer"
-            />
-          </div>
-        </div>
+      <div className="flex mb-5">
+        <label className="relative min-w-[600px] py-3 border-version3-300 border-2 text-sm rounded-lg text-version3-300 cursor-pointer hover:bg-neutral-100 transition-all ease-in-out duration-75 hover:font-bold block">
+          <h1 className="text-center">a. Rp 70.000</h1>
+          <input type="radio" name="choice" className="absolute appearance-none cursor-pointer" />
+        </label>
       </div>
-      <div className="flex justify-center gap-[65px] mt-[88px]">
-        <button className="border-2 border-[#F26800] rounded-lg h-[48px] lg:w-[175px] w-full text-[#F26800]">
-          Sebelumnya
-        </button>
-        <button className="bg-[#FAB317] rounded-lg h-[48px] lg:w-[175px] w-full text-[#FFFF]">
-          Ragu-ragu
-        </button>
-        <button className="bg-[#F26800] rounded-lg h-[48px] lg:w-[175px] w-full text-[#FFFF]">
-          Selanjutnya
-        </button>
+      <div className="flex mb-5">
+        <label className="relative min-w-[600px] py-3 border-version3-300 border-2 text-sm rounded-lg text-version3-300 cursor-pointer hover:bg-neutral-100 transition-all ease-in-out duration-75 hover:font-bold block">
+          <h1 className="text-center">a. Rp 70.000</h1>
+          <input type="radio" name="choice" className="absolute appearance-none cursor-pointer" />
+        </label>
+      </div>
+
+      <div className="flex gap-4 mt-12">
+        <Button
+          type="button"
+          className="flex gap-2 hover:bg-neutral-100/50 items-center px-4 py-2 bg-neutral-50 border-2 border-version3-500 rounded-md shadow-sm"
+        >
+          <MdOutlineNavigateNext className="rotate-180 text-version3-500" />
+          <h1 className="text-version3-500 text-sm font-bold">Sebelumnya</h1>
+        </Button>
+        <Button
+          type="button"
+          className="flex gap-2  items-center px-4 py-2 bg-[#FAB317] border-2 border-[#FAB317] rounded-md shadow-sm hover:bg-[#e1a015]"
+        >
+          <AiOutlineQuestionCircle className=" text-neutral-50" />
+          <h1 className="text-neutral-50 text-sm font-bold">Ragu-ragu</h1>
+        </Button>
+        <Button
+          type="button"
+          className="flex gap-2  items-center px-4 py-2 bg-version3-500 border-2 border-version3-500 rounded-md shadow-sm hover:bg-version2-500"
+        >
+          <h1 className="text-neutral-50 text-sm font-bold">Selanjutnya</h1>
+          <MdOutlineNavigateNext className=" text-neutral-50" />
+        </Button>
       </div>
     </div>
   );
 };
-
-export default Question;
--- apps/lxp/modules/mytraining-quiz/section/StartQuiz/module.tsx
+++ apps/lxp/modules/mytraining-quiz/section/StartQuiz/module.tsx
@@ -1,5 +1,4 @@
-import React, { Fragment } from "react";
-import Question from "./Question";
+import { Question } from "./Question";
 import Count from "./Count";
 import { BreadCrumb } from "@mknows-frontend-services/components/atoms";
 import { myTrainingQuizBreadCumbs } from "../../constant";
@@ -8,9 +7,9 @@ import { ClientProvider } from "../../../common/provider";
 export const QuizStartPage = () => {
   return (
     <ClientProvider>
-      <BreadCrumb items={myTrainingQuizBreadCumbs} />
-      <div className="mx-8 md:mx-14 lg:mx-16 bg-[#FFFF] mt-5 rounded-md">
-        <div className="flex px-[32px] py-[52px] lg:gap-[52px]">
+      <BreadCrumb items={myTrainingQuizBreadCumbs} textColor="text-version2-400" />
+      <div className="mx-8 md:mx-14 lg:mx-16 bg-neutral-50 mt-4 rounded-md shadow-sm">
+        <div className="flex p-8  lg:gap-[52px]">
           <Question />
           <Count />
         </div>
