6947eab98c3e5b8d8c77f4574d56d555e1e1c456
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-05-31 16:09:10 +0700

    create type my-training module

---
+++ apps/lxp/modules/mytraining-module/type.d.ts
@@ -0,0 +1,11 @@
+export interface MyTrainingModuleItem {
+  id: string;
+  total_videos: string;
+  total_documents: string;
+  description: string;
+  is_all_video_seen: boolean;
+  submitted: boolean;
+  batch_id: string;
+}
+
+export type TMyTrainingModuleResponse = ICommonMetaResponse<MyTrainingModuleItem>;
