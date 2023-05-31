a8aa18058c260ed307d4efb1378f311383249f35
Fenny Oktaviani <fnny04@gmail.com>
2023-05-31 14:59:57 +0700

    improvement : add metha data response TMetaCharacter

--- apps/cs/src/modules/user/add-data/types.ts
+++ apps/cs/src/modules/user/add-data/types.ts
@@ -29,3 +29,21 @@ export type TCapabilityFile = {
 };
 
 export type TCapabilityResponse = TMetaResponseSingle<TCapabilityFile>;
+
+export type TCharacterFile = {
+  ktp?: File;
+  selfie?: File;
+  surat_nomor_induk_berusaha?: File;
+  surat_izin_usaha_perdagangan?: File;
+  surat_nomor_akta_notaris?: File;
+  surat_keterangan_domisili_usaha: File;
+  npwp: File;
+  surat_tanda_daftar_perusahaan: File;
+  surat_laporan_keuangan: File;
+  form_credit_applicant: File;
+  surat_kepemilikan_kendaraan: File;
+  nota_kepemilikan_rumah: File;
+  nota_kepemilikan_saham: File;
+};
+
+export type TCharacterResponse = TMetaResponseSingle<TCharacterFile>;
