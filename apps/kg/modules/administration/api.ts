5ffcaaf76d90753eb6566de38d08454b928252e3
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-05-31 14:06:13 +0630

    improvement: add get all in administrasi

--- apps/kg/modules/administration/api.ts
+++ apps/kg/modules/administration/api.ts
@@ -6,18 +6,19 @@ import {
   TFamilyResponse,
   TFileAdm,
   TFileResponse,
+  TConstantsResponse,
 } from "./types";
 import { serialize } from "object-to-formdata";
+
+export const getAdmRequest = async (): Promise<TBiodataResponse> => {
+  const { data } = await api.get("/administration");
+  return data;
+};
 
 export const privateInformationRequest = async (
   payload: TBiodataAdm,
 ): Promise<TBiodataResponse> => {
   const { data } = await api.post("/administration/biodata", payload);
-  return data;
-};
-
-export const privateInformationGetRequest = async (): Promise<TBiodataResponse> => {
-  const { data } = await api.get("/administration");
   return data;
 };
 
@@ -28,5 +29,10 @@ export const familyInformationRequest = async (payload: TFamilyAdm): Promise<TFa
 
 export const fileInformationRequest = async (payload: TFileAdm): Promise<TFileResponse> => {
   const { data } = await api.post("/administration/file", serialize(payload));
+  return data;
+};
+
+export const constantsRequest = async (): Promise<TConstantsResponse> => {
+  const { data } = await api.get("/administration/constants");
   return data;
 };
