ac12296e749947c1ff051ba27363899c75574d6d
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-05-31 14:06:42 +0630

    improvement: add index number in leaderboard

--- apps/kg/modules/leaderboard/section/ranking-bottom.tsx
+++ apps/kg/modules/leaderboard/section/ranking-bottom.tsx
@@ -16,16 +16,16 @@ export const RankingSection: FC = (): ReactElement => {
         ?.sort((a, b) => {
           return b.poin - a.poin;
         })
-        ?.map((x, y) => {
+        ?.map((x, index) => {
           console.log(x);
           return (
-            y >= 3 && (
+            index >= 3 && (
               <>
-                <div key={y} id={`${y}`}>
+                <div key={index}>
                   <div className="flex lg:px-[56px] px-3 mt-[15px] ">
                     <div className="w-full dark:bg-[#222529] bg-white flex justify-between dark:border-b-[#222529] border-b-[#F5F5F5] border-b-2 px-[20px] rounded-[8px]">
                       <div className="flex items-center gap-[30px] p-2">
-                        <h1 className="font-[400] lg:text-[24px] text-[20px]">{x.poin}</h1>
+                        <h1 className="font-[400] lg:text-[24px] text-[20px]">{index + 1}</h1>
                         <Image
                           src={x?.author?.avatar === null ? Avatar : x?.author?.avatar}
                           width={30}
