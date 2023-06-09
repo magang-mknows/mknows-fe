e4b452b6f53bbeec5e4b99a69b7b56e435390564
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-06-09 19:24:16 +0700

    fix: kg - validasi password

--- apps/kg/modules/auth/register/module.tsx
+++ apps/kg/modules/auth/register/module.tsx
@@ -25,7 +25,13 @@ const validationSchema = z
       message: "Email harus valid",
     }),
     full_name: z.string().min(2, { message: "Nama Lengkap harus diisi" }),
-    password: z.string().min(1, { message: "Password harus diisi" }),
+    password: z
+      .string()
+      .min(1, { message: "Password harus diisi" })
+      .min(7, { message: "Password harus diisi" })
+      .min(8, { message: "Password setidaknya ada 8 karakter" })
+      .refine((data) => data.match(/[A-Z]/g), { message: "Password harus mengandung huruf besar" })
+      .refine((data) => data.match(/[0-9]/g), { message: "Password harus mengandung angka" }),
     password_confirmation: z.string().min(1, { message: "Konfirmasi kata sandi harus disisi" }),
   })
   .refine((data) => data.password === data.password_confirmation, {
