41057ae5c7be5ea6b0f33f7e2bf0e541d9b3352c
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-06-09 22:53:01 +0700

    feature : submit quiz answer

--- apps/lxp/modules/mytraining-quiz/section/StartQuiz/api.ts
+++ apps/lxp/modules/mytraining-quiz/section/StartQuiz/api.ts
@@ -1,12 +1,12 @@
 import api from "../../../../services/api";
-import { IQuizAnswerPayload, TGetQuizParams, TQuizQuestionResponse } from "./types";
+import { TGetQuizParams, TQuestionsAnswer, TQuizQuestionResponse } from "./types";
 
 export const getQuizQuestion = async (params: TGetQuizParams): Promise<TQuizQuestionResponse> => {
-  const { data } = await api.get(`/my-works/quiz/${params.quizId}/batch/${params.quizId}/take`);
+  const { data } = await api.get(`/my-works/quiz/${params.quizId}/batch/${params.batchId}/take`);
   return data;
 };
 
-export const submitQuizAnswer = async (params: TGetQuizParams, payload: IQuizAnswerPayload) => {
+export const submitQuizAnswer = async (params: TGetQuizParams, payload: TQuestionsAnswer) => {
   const { data } = await api.post(
     `/my-works/quiz/${params.quizId}/batch/${params.batchId}/submit`,
     payload,
