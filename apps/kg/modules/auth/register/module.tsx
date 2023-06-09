753ab5b0eb5d04e9d4ead81d0a3788cf7f626744
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-06-09 19:31:35 +0700

    fix: typo di alert registrasi

--- apps/kg/modules/auth/register/module.tsx
+++ apps/kg/modules/auth/register/module.tsx
@@ -32,7 +32,7 @@ const validationSchema = z
       .min(8, { message: "Password setidaknya ada 8 karakter" })
       .refine((data) => data.match(/[A-Z]/g), { message: "Password harus mengandung huruf besar" })
       .refine((data) => data.match(/[0-9]/g), { message: "Password harus mengandung angka" }),
-    password_confirmation: z.string().min(1, { message: "Konfirmasi kata sandi harus disisi" }),
+    password_confirmation: z.string().min(1, { message: "Konfirmasi kata sandi harus diisi" }),
   })
   .refine((data) => data.password === data.password_confirmation, {
     message: "Konfirmasi kata sandi tidak valid",
