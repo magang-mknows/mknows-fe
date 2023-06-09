d8191b0f3569f37463f7b8a0af88edf38d055002
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-06-10 02:19:54 +0700

    fix : error build on kg

--- apps/kg/modules/study-contract/details/draft-krs/api.ts
+++
@@ -1,9 +0,0 @@
-import api from "../../../../services/api";
-import { TDraftSubject, TDraftPayload } from "./type";
-
-export const draftRequest = async (
-    payload: TDraftPayload,
-  ): Promise<TDraftSubject> => {
-    const { data } = await api.post(`/student-subject/draft/${payload.id}`, payload);
-    return data;
-  };
