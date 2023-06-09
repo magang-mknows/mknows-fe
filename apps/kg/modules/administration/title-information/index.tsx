1b68bc8966efb7f674addfede325d189d606d805
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-06-09 14:10:45 +0630

    improvement: change condition banner

--- apps/kg/modules/administration/title-information/index.tsx
+++ apps/kg/modules/administration/title-information/index.tsx
@@ -1,29 +1,34 @@
-import { useAdministrationStatus } from "../hooks";
+import { useGetAllAdministration } from "../hooks";
 import React, { FC, ReactElement } from "react";
 import { RiErrorWarningFill } from "react-icons/ri";
 
 export const TitleAdministration: FC = (): ReactElement => {
-  const { getAdministrationStatus } = useAdministrationStatus();
+  const { data } = useGetAllAdministration();
+  const getAdministrationStatus = data?.data;
 
   return (
     <div className="title lg:px-20 px-5 w-full">
       <h1 className="text-[20px] font-[600]  text-black md:text-left mb-7">Administrasi</h1>
       <div
         className={`${
-          getAdministrationStatus === "none" ? "bg-yellow-100" : "bg-primary-100"
+          getAdministrationStatus?.status === "NOT-SUBMITTED" ? "bg-yellow-100" : "bg-primary-100"
         } w-full flex items-center gap-x-4 px-4 py-2.5 rounded-lg`}
       >
         <RiErrorWarningFill
           className={`${
-            getAdministrationStatus === "none" ? "text-yellow-500" : "text-primary-500"
+            getAdministrationStatus?.status === "NOT-SUBMITTED"
+              ? "text-yellow-500"
+              : "text-primary-500"
           } `}
         />
         <p
           className={`${
-            getAdministrationStatus === "none" ? "text-yellow-500" : "text-primary-500"
+            getAdministrationStatus?.status === "NOT-SUBMITTED"
+              ? "text-yellow-500"
+              : "text-primary-500"
           } `}
         >
-          {getAdministrationStatus === "finished"
+          {getAdministrationStatus?.status === "PENDING"
             ? "Menunggu Persetujuan Approve"
             : "Kamu harus mengisi administrasi untuk bisa mengambil rencana studi."}
         </p>
