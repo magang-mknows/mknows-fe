ceb43cf4a9bfa98e32f41f4e73dcc194905d0d7e
Nandi Irawan <nandiirawan327@gmail.com>
2023-06-11 17:10:37 +0700

    feature: add breadcrumb and fix integration issue in quiz-desc

--- apps/kg/modules/mystudy/quiz/module.tsx
+++ apps/kg/modules/mystudy/quiz/module.tsx
@@ -9,15 +9,13 @@ import { TQuizDescItem } from "./types";
 export const QuizModule: FC = (): ReactElement => {
   const router = useRouter();
 
-  const { data } = useGetQuizDescById(router.query.quizId as unknown as string);
+  const { data } = useGetQuizDescById(router.query.sessionId as string);
   const dataQuizDesc = data?.data as unknown as TQuizDescItem;
 
   return (
     <div className="flex flex-col items-center gap-y-4 min-h-[705px] px-[20px] md:px-[88px] mt-8 mb-15">
       <div className="flex flex-col items-center gap-y-[18px] text-center">
-        <h1 className="text-4xl font-semibold text-[#171717]">
-          Quiz Manajemen Keuangan {/* it has to be from api but... */}
-        </h1>
+        <h1 className="text-4xl font-semibold text-[#171717]">Quiz {router.query.subjectName}</h1>
         <p className="text-2xl font-normal text-[#737373]">Pertemuan 1</p>
       </div>
       <div className="flex flex-col md:flex-row min-h-[550px] items-center gap-11 text-[#171717]">
@@ -36,18 +34,20 @@ export const QuizModule: FC = (): ReactElement => {
             </p>
             <p className="">Beberapa ketentuannya sebagai berikut:</p>
             <p className="">
-              Terdapat <span className="font-bold">10 pertanyaan</span> yang harus dikerjakan dalam
-              Quiz ini dan Setiap pertanyaan mempunyai bobot Nilai{" "}
-              <span className="font-bold">10 Poin</span>
+              Terdapat <span className="font-bold">{dataQuizDesc.total_questions} pertanyaan</span>{" "}
+              yang harus dikerjakan dalam Quiz ini dan Setiap pertanyaan mempunyai bobot Nilai{" "}
+              <span className="font-bold">{dataQuizDesc.question_point} Poin</span>
             </p>
-            <p className="font-bold">Syarat Nilai Kelulusan : 80 Poin</p>
-            <p className="font-bold">Durasi Ujian : 10 menit</p>
+            <p className="font-bold">Syarat Nilai Kelulusan : {dataQuizDesc.kkm} Poin</p>
+            <p className="font-bold">Durasi Ujian : {dataQuizDesc.duration / 60} menit</p>
             <p className="">
               Setelah dimulai, quiz tidak dapat dihentikan, anda harus menyelesaikan quiz hingga
               selesai
             </p>
             <p className="">
-              Anda mempunyai <span className="font-bold">3 kali kesempatan</span> mengulang quiz
+              Anda mempunyai{" "}
+              <span className="font-bold">{dataQuizDesc.remaining_attempt} kali kesempatan</span>{" "}
+              mengulang quiz
             </p>
             <p className="">
               Apabila tidak memenuhi syarat kelulusan, maka Anda harus menunggu selama 1 menit untuk
@@ -55,11 +55,33 @@ export const QuizModule: FC = (): ReactElement => {
             </p>
             <p className="">Selamat Mengerjakan!</p>
           </div>
-          <Link href={`${router.asPath}/ambil/${dataQuizDesc?.id}`}>
-            <button className="w-full h-[42px] lg:w-[328px] lg:h-[56px] text-[16px] font-medium bg-[#106FA4] text-white  flex gap-x-2 rounded justify-center items-center hover:opacity-75 duration-200">
-              Mulai Quiz (Sisa {dataQuizDesc?.remaining_attempt || "unknown"})
-            </button>
-          </Link>
+
+          <div className="w-full flex flex-col gap-y-2">
+            {dataQuizDesc.remaining_attempt > 0 && (
+              <Link href={`${router.asPath}/ambil/${dataQuizDesc?.id}`}>
+                <button className="w-full h-[42px] lg:w-[328px] lg:h-[48px] text-[16px] font-medium bg-[#106FA4] text-white  flex gap-x-2 rounded justify-center items-center hover:opacity-75 duration-200">
+                  Mulai Quiz (Sisa {dataQuizDesc.remaining_attempt})
+                </button>
+              </Link>
+            )}
+
+            {dataQuizDesc.remaining_attempt === 0 && (
+              <button
+                disabled={dataQuizDesc.remaining_attempt === 0}
+                className="w-full h-[42px] lg:w-[328px] lg:h-[48px] text-[16px] font-medium bg-neutral-400 text-white  flex gap-x-2 rounded justify-center items-center hover:opacity-75 duration-200"
+              >
+                Mulai Quiz (Sisa {dataQuizDesc.remaining_attempt})
+              </button>
+            )}
+
+            {dataQuizDesc.remaining_attempt > 0 && dataQuizDesc.remaining_attempt < 3 && (
+              <Link href={`${router.asPath}/skor/${dataQuizDesc?.id}`}>
+                <button className="w-full h-[42px] lg:w-[328px] lg:h-[48px] text-[16px] font-medium border-2 border-[#106FA4] text-[#106FA4] flex gap-x-2 rounded justify-center items-center hover:opacity-75 duration-200">
+                  Lihat Riwayat
+                </button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </div>
