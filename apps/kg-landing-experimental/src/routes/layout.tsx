e3fb73c1ca563354d1a75fa489bc287b4cfd8445
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-06-13 15:52:22 +0700

    feature: kampus gratis dosen

--- apps/kg-landing-experimental/src/routes/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { component$, Slot } from "@builder.io/qwik";
-
-export default component$(() => {
-  return (
-    <section class="flex items-center w-full h-screen justify-center">
-      <Slot />
-    </section>
-  );
-});
