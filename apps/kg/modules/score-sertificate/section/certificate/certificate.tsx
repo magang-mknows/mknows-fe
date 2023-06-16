4549c58b9ad32c2d6b880cd2c65bb2074cd426c3
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-06-16 16:18:09 +0630

    improvement: change color config

--- apps/kg/modules/score-sertificate/section/certificate/certificate.tsx
+++ apps/kg/modules/score-sertificate/section/certificate/certificate.tsx
@@ -20,45 +20,45 @@ export const CertificateSection = (): ReactElement => {
     <div className="w-full">
       <div className="flex justify-end  ">
         <button
-          className="w-[109px] h-[40px] rounded-[8px] text-center border-[3px] border-[#106FA4] relative "
+          className="w-[109px] h-[40px] rounded-[8px] text-center border-[3px] border-primary-500 relative "
           onClick={() => setOpen(open == true ? false : true)}
         >
           <div className="my-auto flex justify-center gap-[10px] items-center">
-            <p className="text-[#106FA4] text-[14px] dark:text-white">Filter</p>
-            <RxSlider className="text-[#106FA4] dark:text-white" />
+            <p className="text-primary-500 text-[14px] dark:text-white">Filter</p>
+            <RxSlider className="text-primary-500 dark:text-white" />
           </div>
           {open && (
             <div className="flex justify-end container mt-[10px] z-[9000] absolute w-[150px] -bottom-32 ease-in-out ">
               <div className="w-[160px] h-[114px] rounded-[8px] text-center  overflow-hidden shadow-2xl ">
                 <button
-                  className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
+                  className="flex justify-start h-[38px] relative hover:bg-neutral-100 w-full bg-white "
                   onClick={() => setFilter("SEMUA")}
                 >
                   <div
                     className={`h-full w-[5px] ${
-                      filter == "SEMUA" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
-                    } absolute left-0 overflow-hidden hover:hover:bg-[#F5F5F5]`}
+                      filter == "SEMUA" ? "bg-blue-100" : "bg-white"
+                    } absolute left-0 overflow-hidden hover:bg-neutral-100`}
                   ></div>
                   <p className="text-[#262626] text-[14px] my-auto ml-3">Semua</p>
                 </button>
                 <button
-                  className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
+                  className="flex justify-start h-[38px] relative hover:bg-neutral-100 w-full bg-white "
                   onClick={() => setFilter("SUBJECT")}
                 >
                   <div
                     className={`h-full w-[5px] ${
-                      filter == "SUBJECT" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
-                    } absolute left-0 overflow-hidden hover:hover:bg-[#F5F5F5]`}
+                      filter == "SUBJECT" ? "bg-blue-100" : "bg-white"
+                    } absolute left-0 overflow-hidden hover:bg-neutral-100`}
                   ></div>
                   <p className="text-[#262626] text-[14px] my-auto ml-3">Mata Kuliah</p>
                 </button>
                 <button
-                  className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
+                  className="flex justify-start h-[38px] relative hover:bg-neutral-100 w-full bg-white "
                   onClick={() => setFilter("EVENT")}
                 >
                   <div
                     className={`h-full w-[5px] ${
-                      filter == "EVENT" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
+                      filter == "EVENT" ? "bg-blue-100" : "bg-white"
                     } absolute left-0 overflow-hidden`}
                   ></div>
                   <p className="text-[#262626] text-[14px] my-auto ml-3">Webinar</p>
@@ -85,10 +85,10 @@ export const CertificateSection = (): ReactElement => {
                   onClick={() => setPopupStatus(true)}
                 >
                   <div className="w-full mt-[12px]">
-                    <h1 className="text-center text-[#171717] text-[18px] font-[500] dark:text-white">
+                    <h1 className="text-center text-neutral-900 text-[18px] font-[500] dark:text-white">
                       {item?.event_certificate?.name}
                     </h1>
-                    <p className="text-center text-[#A3A3A3] dark:text-white text-[16px] font-[500]">
+                    <p className="text-center text-neutral-base dark:text-white text-[16px] font-[500]">
                       {item?.type === "EVENT"
                         ? "Webinar"
                         : item?.type === "SUBJECT"
@@ -107,7 +107,7 @@ export const CertificateSection = (): ReactElement => {
                   >
                     <div className="flex md:flex-row flex-col md:gap-3 gap-0 items-center ">
                       <Link href={item?.link} download>
-                        <button className="w-[230px] h-[56px]  bg-[#106FA4] rounded-[8px] text-white mb-3 md:mb-0">
+                        <button className="w-[230px] h-[56px] bg-primary-500 rounded-[8px] text-white mb-3 md:mb-0">
                           <div className=" flex justify-center gap-3">
                             <div>
                               <Image src={download} alt={"download-icon"} />
@@ -119,7 +119,7 @@ export const CertificateSection = (): ReactElement => {
                       </Link>
 
                       <button
-                        className="w-[230px] h-[56px] border-[#106FA4] border-2 rounded-[8px] text-[#106FA4] "
+                        className="w-[230px] h-[56px] border-primary-500 border-2 rounded-[8px] text-primary-500 "
                         onClick={() => setPopupStatus(false)}
                       >
                         Kembali
@@ -134,11 +134,11 @@ export const CertificateSection = (): ReactElement => {
           <div className=" col-span-12 grid place-content-center  ">
             <Image src={ImageNull} alt="image-null" className="w-full" />
             {filter === "SUBJECT" ? (
-              <p className="text-center text-[#171717] text-[18px] font-[500] dark:text-white">
+              <p className="text-center text-neutral-900 text-[18px] font-[500] dark:text-white">
                 Belum ada matakuliah yang diselesaikan
               </p>
             ) : (
-              <p className="text-center text-[#171717] text-[18px] font-[500] dark:text-white">
+              <p className="text-center text-neutral-900 text-[18px] font-[500] dark:text-white">
                 Kamu belum mengikuti webinar
               </p>
             )}
@@ -154,16 +154,14 @@ export const CertificateSection = (): ReactElement => {
                     src={item?.thumbnail_link}
                     imgwidth={100}
                     imgheight={100}
-                    className={
-                      "container mx-auto w-full h-full rounded-[8px] p-[12px] bg-white dark:bg-[#08405e] "
-                    }
+                    className={"container mx-auto w-full h-full rounded-[8px] p-[12px] bg-white"}
                     onClick={() => setPopupStatus(true)}
                   >
                     <div className="w-full mt-[12px]">
-                      <h1 className="text-center text-[#171717] text-[18px] font-[500] dark:text-white">
+                      <h1 className="text-center text-neutral-900 text-[18px] font-[500] dark:text-white">
                         {item?.event_certificate?.name}
                       </h1>
-                      <p className="text-center text-[#A3A3A3] text-[16px] font-[500] dark:text-white">
+                      <p className="text-center text-neutral-base text-[16px] font-[500] dark:text-white">
                         {item?.type === "EVENT"
                           ? "Webinar"
                           : item?.type === "SUBJECT"
@@ -182,7 +180,7 @@ export const CertificateSection = (): ReactElement => {
                     >
                       <div className="flex md:flex-row flex-col md:gap-3 gap-0 items-center ">
                         <Link href={item?.link} download>
-                          <button className="w-[230px] h-[56px]  bg-[#106FA4] rounded-[8px] text-white mb-3 md:mb-0">
+                          <button className="w-[230px] h-[56px] bg-primary-500 rounded-[8px] text-white mb-3 md:mb-0">
                             <div className=" flex justify-center gap-3">
                               <div>
                                 <Image src={download} alt={"download-icon"} />
@@ -194,7 +192,7 @@ export const CertificateSection = (): ReactElement => {
                         </Link>
 
                         <button
-                          className="w-[230px] h-[56px] border-[#106FA4] border-2 rounded-[8px] text-[#106FA4] "
+                          className="w-[230px] h-[56px] border-primary-500 border-2 rounded-[8px] text-primary-500 "
                           onClick={() => setPopupStatus(false)}
                         >
                           Kembali
