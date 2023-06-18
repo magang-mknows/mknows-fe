056b118c7d5245d2e9a117c2573cae56c971bc34
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-06-18 20:13:00 +0700

    fix bg color breadcrumb guide

--- apps/lxp/modules/guide-guide dictinoary/module.tsx
+++ apps/lxp/modules/guide-guide dictinoary/module.tsx
@@ -31,7 +31,7 @@ export const GuideDictinoaryModuleId: NextPage = (): ReactElement => {
 
   return (
     <ClientProvider sectionClassName="!bg-[#FAFAFA]">
-      <BreadCrumb items={BreadCrumbEndpoint} textColor="text-version2-400 !bg-white" />
+      <BreadCrumb items={BreadCrumbEndpoint} textColor="text-version2-400" bgColor="!bg-white" />
       <div className="w-full flex flex-col-reverse lg:flex-row justify-start px-8 md:px-14 lg:px-16 gap-36">
         <Sidebar />
         <ContentSection />
