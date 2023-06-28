b9881c5d52f347c668e1fea89feec63cd6458ee5
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-06-28 22:21:11 +0700

    improvement: change color guide video in color config

--- apps/kg/modules/guide/registration-video/description/description.tsx
+++ apps/kg/modules/guide/registration-video/description/description.tsx
@@ -5,8 +5,8 @@ export const Description = (props: videoGuideTypes): ReactElement => {
   const { titleVideo, descVideo } = props;
   return (
     <div className="bg-white w-full h-fit rounded-[8px] mt-[28px] shadow-md py-8 px-[46px] mb-10">
-      <h1 className="text-[18px] font-[600] text-[#171717]  h-269px ">{titleVideo}</h1>
-      <p className="text-[12px] font-[400] text-[#737373]  h-269px pt-[20px] ">{descVideo}</p>
+      <h1 className="text-[18px] font-[600] text-neutral-900  h-269px ">{titleVideo}</h1>
+      <p className="text-[12px] font-[400] text-neutral-500  h-269px pt-[20px] ">{descVideo}</p>
     </div>
   );
 };
