ddfd3e4012996f8b804cd1e8b194b9b5c7d1e9b0
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-06-28 22:19:48 +0700

    improvement: change color guide book in color config

--- apps/kg/modules/guide/guide-book/hero/hero.tsx
+++ apps/kg/modules/guide/guide-book/hero/hero.tsx
@@ -9,20 +9,20 @@ export const HeroSection = (): ReactElement => {
       <div className="col-span-1 ">
         <div className="title">
           <h1 className="text-[50px] -ml-1 font-[700]">Buku panduan </h1>
-          <p className="text-[14px] font-[400] text-[#737373] dark:text-white">
+          <p className="text-[14px] font-[400] text-neutral-500 dark:text-white">
             Berupa buku petunjuk penggunaan dan pengertian fitur- fitur yang ada di Website KG
             secara terperinci (isinya lebih detail jika dibandingkan video tutorial). Menjelaskan
             juga keunggulan dan manfaat fitur tersebut.
           </p>
         </div>
-        <div className="seacrh lg:w-[700px] h-[56px] w-full bg-[#F5F5F5] dark:bg-[#232529] rounded-[8px] mt-[48px] flex items-center gap-4  ">
+        <div className="seacrh lg:w-[700px] h-[56px] w-full bg-neutral-100  rounded-[8px] mt-[48px] flex items-center gap-4  ">
           <Image src={search} alt={"search"} className={"ml-[24px]"} />
           <input
             type="text"
             name=""
             id=""
             placeholder="Cari informasi"
-            className=" bg-[#F5F5F5] w-full focus:outline-none"
+            className=" bg-neutral-100 w-full focus:outline-none"
           />
         </div>
       </div>
