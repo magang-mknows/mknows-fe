aeb62e84dfba683c9cd5732ed7f48d481b8cd3d1
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-06-28 22:21:03 +0700

    improvement: change color guide registration book in color config

--- apps/kg/modules/guide/registration-book/hero/hero.tsx
+++ apps/kg/modules/guide/registration-book/hero/hero.tsx
@@ -10,7 +10,7 @@ export const HeroSection = (): ReactElement => {
           Panduan pendaftaran
         </h1>
       </div>
-      <div className="mt-[36px] bg-[#FEDBD7] w-full h-[135px] container mx-auto rounded-[8px] lg:mb-[30px] mb-[60px]   ">
+      <div className="mt-[36px] bg-error-100 w-full h-[135px] container mx-auto rounded-[8px] lg:mb-[30px] mb-[60px]   ">
         <div className="container px-[36px] lg:pt-[28px] lg:py-[16px] py-[10px]">
           <h1 className="text-[18px] font-[700] dark:text-black ">Penting !</h1>
           <div className="flex mt-[10px] gap-[17px] items-center ">
