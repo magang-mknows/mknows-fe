e7aadfd1e89dd5f8431a64943b044eae1f491819
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-06-28 21:22:47 +0700

    improvement: bug qa font guide video

--- apps/kg/modules/guide/guide-video/hero/hero.tsx
+++ apps/kg/modules/guide/guide-video/hero/hero.tsx
@@ -8,7 +8,7 @@ export const HeroSection = (): ReactElement => {
     <div className="lg:grid lg:grid-cols-2 px-6 md:px-8 w-full lg:px-16 mt-[10px] mb-[50px]">
       <div className="col-span-1 ">
         <div className="title">
-          <h1 className=" md:text-[50px] text-[40px] font-extrabold">Video panduan </h1>
+          <h1 className=" md:text-[50px] text-[40px] font-[700]">Video panduan </h1>
           <p className="text-[14px] font-[400] text-[#737373] dark:text-white">
             Berupa video tutorial penggunaan dan fungsi fitur yang ada di Website KG
           </p>
